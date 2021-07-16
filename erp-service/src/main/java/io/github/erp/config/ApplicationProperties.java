f89817f841aeccbc50630e8063ad42a348f989be
Edwin Njeru <mailnjeru@gmail.com>
2021-07-16 18:41:24 +0300

    Initial version of erpService generated by JHipster-6.10.5

---
+++ erp-service/src/main/java/io/github/erp/config/ApplicationProperties.java
@@ -0,0 +1,13 @@
+package io.github.erp.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * Properties specific to Erp Service.
+ * <p>
+ * Properties are configured in the {@code application.yml} file.
+ * See {@link io.github.jhipster.config.JHipsterProperties} for a good example.
+ */
+@ConfigurationProperties(prefix = "application", ignoreUnknownFields = false)
+public class ApplicationProperties {
+}
