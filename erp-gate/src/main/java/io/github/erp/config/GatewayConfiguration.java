235d39172ea8aacdddbb419a13b3e727cfa045c4
Edwin Njeru <mailnjeru@gmail.com>
2021-07-16 18:48:51 +0300

    Create the ERP service

--- erp-gate/src/main/java/io/github/erp/config/GatewayConfiguration.java
+++ erp-gate/src/main/java/io/github/erp/config/GatewayConfiguration.java
@@ -4,8 +4,6 @@ import io.github.jhipster.config.JHipsterProperties;
 
 import io.github.erp.gateway.accesscontrol.AccessControlFilter;
 import io.github.erp.gateway.responserewriting.SwaggerBasePathRewritingFilter;
-import io.github.erp.gateway.ratelimiting.RateLimitingFilter;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cloud.netflix.zuul.filters.RouteLocator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -31,23 +29,4 @@ public class GatewayConfiguration {
         }
     }
 
-    /**
-     * Configures the Zuul filter that limits the number of API calls per user.
-     * <p>
-     * This uses Bucket4J to limit the API calls, see {@link io.github.erp.gateway.ratelimiting.RateLimitingFilter}.
-     */
-    @Configuration
-    @ConditionalOnProperty("jhipster.gateway.rate-limiting.enabled")
-    public static class RateLimitingConfiguration {
-
-        private final JHipsterProperties jHipsterProperties;
-
-        public RateLimitingConfiguration(JHipsterProperties jHipsterProperties) {
-            this.jHipsterProperties = jHipsterProperties;
-        }
-        @Bean
-        public RateLimitingFilter rateLimitingFilter() {
-            return new RateLimitingFilter(jHipsterProperties);
-        }
-    }
 }
