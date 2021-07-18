d5877df44abef4192b584d05672c68f595c11344
Edwin Njeru <mailnjeru@gmail.com>
2021-07-18 09:11:14 +0300

    Configure client connection to the gate

--- erp-client/webpack/webpack.common.js
+++ erp-client/webpack/webpack.common.js
@@ -70,7 +70,7 @@ module.exports = (options) => ({
                 // If this URL is left empty (""), then it will be relative to the current context.
                 // If you use an API server, in `prod` mode, you will need to enable CORS
                 // (see the `jhipster.cors` common JHipster property in the `application-*.yml` configurations)
-                SERVER_API_URL: `''`
+                SERVER_API_URL: `'http://localhost:8790/'`
             }
         }),
         new CopyWebpackPlugin({
