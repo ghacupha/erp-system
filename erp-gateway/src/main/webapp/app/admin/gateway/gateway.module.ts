de01581613c188cb8df0b08eaf2d53428d248984
Edwin Njeru <mailnjeru@gmail.com>
2021-07-21 17:19:13 +0300

    Create uaa based erp-gateway inclusive of the client

---
+++ erp-gateway/src/main/webapp/app/admin/gateway/gateway.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ErpGatewaySharedModule } from 'app/shared/shared.module';
+
+import { GatewayComponent } from './gateway.component';
+
+import { gatewayRoute } from './gateway.route';
+
+@NgModule({
+  imports: [ErpGatewaySharedModule, RouterModule.forChild([gatewayRoute])],
+  declarations: [GatewayComponent],
+})
+export class GatewayModule {}
