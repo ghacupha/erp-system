15d22bd310f8b3fc8bcb5ff51159b6f5bd1aebe1
Edwin Njeru <mailnjeru@gmail.com>
2021-07-21 15:10:06 +0300

    Initial version of erpUaa generated by generator-jhipster@7.1.0

---
+++ delete.erp-uaa/src/main/java/io/github/erp/config/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring Framework configuration files.
+ */
+package io.github.erp.config;
---
+++ delete.erp-uaa/src/test/java/io/github/erp/IntegrationTest.java
@@ -0,0 +1,17 @@
+package io.github.erp;
+
+import io.github.erp.ErpUaaApp;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = ErpUaaApp.class)
+public @interface IntegrationTest {
+}
