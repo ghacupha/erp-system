ab76cafff67e3980d4ed8aa6f98127a2fd00a919
Edwin Njeru <mailnjeru@gmail.com>
2021-07-26 14:50:28 +0300

    Configure tests for computation1

--- erp-service/src/test/java/io/github/erp/modules/PaymentComputation1Test.java
+++ erp-service/src/test/java/io/github/erp/modules/PaymentComputation1Test.java
@@ -1,55 +1,29 @@
 package io.github.erp.modules;
 
 import io.github.erp.modules.models.PaymentCalculationInt;
-import io.github.erp.modules.models.PaymentRequisitionInt;
-import io.github.erp.modules.models.TaxRuleInt;
 import org.javamoney.moneta.Money;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import javax.money.MonetaryAmount;
-import java.math.BigDecimal;
-
+import static org.assertj.core.api.Assertions.assertThat;
 import static io.github.erp.modules.PaymentReferenceCodes.BASE_SYSTEM_CURRENCY_CODE;
-import static org.junit.jupiter.api.Assertions.*;
 
-class PaymentComputation1Test {
+class PaymentComputation1Test extends PaymentComputationTests  {
 
     private PaymentComputation computation;
 
-    static final MonetaryAmount INVOICE_AMOUNT =
-        Money.of(BigDecimal.valueOf(Long.parseLong(String.valueOf(40000))),BASE_SYSTEM_CURRENCY_CODE);
-
-    static final MonetaryAmount DISBURSEMENT_AMOUNT =
-        Money.of(BigDecimal.valueOf(Long.parseLong(String.valueOf(27500))),BASE_SYSTEM_CURRENCY_CODE);
-
-    static final MonetaryAmount VATABLE_AMOUNT =
-        Money.of(BigDecimal.valueOf(Long.parseLong(String.valueOf(513.63))),BASE_SYSTEM_CURRENCY_CODE);
-
-    PaymentRequisitionInt requisition = PaymentRequisitionInt.builder()
-        .invoicedAmount(INVOICE_AMOUNT)
-        .disbursementCost(DISBURSEMENT_AMOUNT)
-        .vatableAmount(VATABLE_AMOUNT)
-        .build();
-
-
-
     @BeforeEach
     void setUp() {
-
-        // todo define tax rules
-        TaxRuleInt taxRule = TaxRuleInt.builder().build();
-
         computation = new PaymentComputation1(taxRule);
     }
 
     @Test
     void calculate() {
-
-
-
         PaymentCalculationInt calculation = computation.calculate(requisition);
 
-        // 
+        assertThat(calculation.getPaymentExpense()).isEqualTo(Money.of(40000, BASE_SYSTEM_CURRENCY_CODE));
+        assertThat(calculation.getWithholdingVAT()).isEqualTo(Money.of(690, BASE_SYSTEM_CURRENCY_CODE));
+        assertThat(calculation.getWithholdingTax()).isEqualTo(Money.of(0, BASE_SYSTEM_CURRENCY_CODE));
+        assertThat(calculation.getPaymentAmount()).isEqualTo(Money.of(39310, BASE_SYSTEM_CURRENCY_CODE));
     }
 }
