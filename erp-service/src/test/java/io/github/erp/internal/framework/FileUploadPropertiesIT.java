32fd5636ffd1b7caf5b701f84ca06a5ec859da57
Edwin Njeru <mailnjeru@gmail.com>
2021-07-27 18:22:21 +0300

    Add excel tests

---
+++ erp-service/src/test/java/io/github/erp/internal/framework/FileUploadPropertiesIT.java
@@ -0,0 +1,49 @@
+package io.github.erp.internal.framework;
+
+/*-
+ * Leassets Server - Leases and assets management platform
+ * Copyright © 2021 Edwin Njeru (mailnjeru@gmail.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import io.github.erp.ErpServiceApp;
+import io.github.erp.config.SecurityBeanOverrideConfiguration;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * This is to test whether the file-upload configurations will be setup at startup
+ */
+@SpringBootTest(classes = { SecurityBeanOverrideConfiguration.class, ErpServiceApp.class })
+@ExtendWith(MockitoExtension.class)
+@AutoConfigureMockMvc
+@WithMockUser
+public class FileUploadPropertiesIT {
+
+    @Autowired
+    private FileUploadsProperties fileUploadsProperties;
+
+    @Test
+    public void whenFactoryProvidedThenYamlPropertiesInjected() {
+        assertThat(fileUploadsProperties.getListSize()).isEqualTo(1500);
+    }
+}
---
+++ erp-service/src/test/java/io/github/erp/internal/framework/excel/ExcelTestUtil.java
@@ -0,0 +1,67 @@
+package io.github.erp.internal.framework.excel;
+
+/*-
+ * Leassets Server - Leases and assets management platform
+ * Copyright © 2021 Edwin Njeru (mailnjeru@gmail.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import com.poiji.option.PoijiOptions;
+import com.poiji.option.PoijiOptions.PoijiOptionsBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.time.format.DateTimeFormatter;
+import java.util.Objects;
+
+public class ExcelTestUtil {
+
+    private final static Logger log = LoggerFactory.getLogger(ExcelTestUtil.class);
+
+    public static byte[] toBytes(File file) throws IOException {
+
+        return Files.readAllBytes(file.toPath());
+    }
+
+
+    public static PoijiOptions getDefaultPoijiOptions() {
+
+        // @formatter:off
+        return PoijiOptionsBuilder.settings()
+                           .ignoreHiddenSheets(true)
+                           .preferNullOverDefault(true)
+                           .datePattern("yyyy/MM/dd")
+                           .dateTimeFormatter(DateTimeFormatter.ISO_DATE_TIME)
+                           .build();
+        // @formatter:on
+    }
+
+    public static File readFile(String filename) {
+
+        log.info("\nReading file : {}...", filename);
+
+    // @formatter:off
+    return new File(
+            Objects.requireNonNull(
+                ClassLoader.getSystemClassLoader()
+                           .getResource("files/" + filename))
+                   .getFile());
+    // @formatter:on
+    }
+
+}
