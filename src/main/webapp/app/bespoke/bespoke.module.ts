d73c9dc88a1a604d96c0283fbedb90d09ed6e8cd
Edwin Njeru <mailnjeru@gmail.com>
2021-08-10 16:37:16 +0300

    Remove payments bespoke module

--- src/main/webapp/app/bespoke/bespoke.module.ts
+++ src/main/webapp/app/bespoke/bespoke.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutModule } from 'app/bespoke/about/about.module';
-import {PaymentsMaintenanceModule} from "app/bespoke/payments/payments-maintenance.module";
 import { SharedModule } from '../shared/shared.module';
 
 const routes: Routes = [
@@ -10,14 +9,10 @@ const routes: Routes = [
     path: 'about',
     loadChildren: () => import('./about/about.module').then(m => m.AboutModule),
   },
-  {
-    path: '',
-    loadChildren: () => import('./payments/payments-maintenance.module').then(m => m.PaymentsMaintenanceModule),
-  },
 ];
 
 @NgModule({
-  imports: [CommonModule, SharedModule, RouterModule.forChild(routes), AboutModule, PaymentsMaintenanceModule],
-  exports: [AboutModule, PaymentsMaintenanceModule],
+  imports: [CommonModule, SharedModule, RouterModule.forChild(routes), AboutModule],
+  exports: [AboutModule],
 })
 export class BespokeModule {}
