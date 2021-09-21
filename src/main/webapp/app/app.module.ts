5cb0d015fc5bbd48a2b71cd5c52ca0881d8607d7
Edwin Njeru <mailnjeru@gmail.com>
2021-09-21 10:38:58 +0300

    Implement about-page

--- src/main/webapp/app/app.module.ts
+++ src/main/webapp/app/app.module.ts
@@ -24,12 +24,14 @@ import { NavbarComponent } from './layouts/navbar/navbar.component';
 import { FooterComponent } from './layouts/footer/footer.component';
 import { PageRibbonComponent } from './layouts/profiles/page-ribbon.component';
 import { ErrorComponent } from './layouts/error/error.component';
+import {ErpSystemModule} from "./erp/erp-system.module";
 
 @NgModule({
   imports: [
     BrowserModule,
     SharedModule,
     HomeModule,
+    ErpSystemModule,
     // jhipster-needle-angular-add-module JHipster will add new module here
     EntityRoutingModule,
     AppRoutingModule,
