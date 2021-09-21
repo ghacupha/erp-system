d18d64c9a1641bdd362a18f0c08c567e21fdc52b
Edwin Njeru <mailnjeru@gmail.com>
2021-09-21 11:15:56 +0300

    Implement base-nagivation for current entities

--- src/main/webapp/app/erp/erp-nav/erp-navigation.module.ts
+++ src/main/webapp/app/erp/erp-nav/erp-navigation.module.ts
@@ -1,11 +1,23 @@
 import {NgModule} from "@angular/core";
 import {SharedModule} from "../../shared/shared.module";
 import {CommonModule} from "@angular/common";
+import {ErpFilesNavigationComponent} from "./files/erp-files-navigation.component";
+import {RouterModule} from "@angular/router";
+import {PaymentsNavComponent} from "./payment-nav/payments-nav.component";
 
 @NgModule({
+  declarations: [
+    ErpFilesNavigationComponent,
+    PaymentsNavComponent
+  ],
   imports: [
     SharedModule,
-    CommonModule
+    CommonModule,
+    RouterModule
+  ],
+  exports: [
+    ErpFilesNavigationComponent,
+    PaymentsNavComponent
   ]
 })
 export class ErpNavigationModule {}
