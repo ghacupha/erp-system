535b973566fdb8021a38b17dc42ef7a8c3fc1fb8
Edwin Njeru <mailnjeru@gmail.com>
2021-09-23 16:50:23 +0300

    Implement indexing service for common entities

---
+++ src/main/java/io/github/erp/erp/DealersIndexingService.java
@@ -0,0 +1,35 @@
+package io.github.erp.erp;
+
+import com.google.common.collect.ImmutableList;
+import io.github.erp.repository.DealerRepository;
+import io.github.erp.repository.SignedPaymentRepository;
+import io.github.erp.repository.search.DealerSearchRepository;
+import io.github.erp.repository.search.SignedPaymentSearchRepository;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class DealersIndexingService  extends AbtractStartUpIndexService implements ApplicationIndexingService, ApplicationListener<ApplicationReadyEvent> {
+
+    private final DealerRepository repository;
+    private final DealerSearchRepository searchRepository;
+
+    public DealersIndexingService(DealerRepository repository, DealerSearchRepository searchRepository) {
+        this.repository = repository;
+        this.searchRepository = searchRepository;
+    }
+
+    @Async
+    @Override
+    public void index() {
+        this.searchRepository.saveAll(
+            this.repository.findAll()
+                .stream()
+                .filter(entity -> !searchRepository.existsById(entity.getId()))
+                .collect(ImmutableList.toImmutableList()));
+    }
+}
