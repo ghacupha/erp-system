efc613bf00cdac015e3b0e0cdeb3071403f37697
Edwin Njeru <mailnjeru@gmail.com>
2021-09-27 15:53:33 +0300

    Create a several dealers module

---
+++ src/main/webapp/app/erp/erp-pages/dealers/dealer/dealer.model.ts
@@ -0,0 +1,46 @@
+import {IPaymentRequisition} from '../../payments/payment-requisition/payment-requisition.model';
+import {IPaymentLabel} from '../../payment-label/payment-label.model';
+import {ISignedPayment} from '../../signed-payment/signed-payment.model';
+import {IPlaceholder} from '../../../../entities/erpService/placeholder/placeholder.model';
+
+export interface IDealer {
+  id?: number;
+  dealerName?: string;
+  taxNumber?: string | null;
+  postalAddress?: string | null;
+  physicalAddress?: string | null;
+  accountName?: string | null;
+  accountNumber?: string | null;
+  bankersName?: string | null;
+  bankersBranch?: string | null;
+  bankersSwiftCode?: string | null;
+  paymentLabels?: IPaymentLabel[] | null;
+  dealerGroup?: IDealer | null;
+  paymentRequisitions?: IPaymentRequisition[] | null;
+  signedPayments?: ISignedPayment[] | null;
+  placeholders?: IPlaceholder[] | null;
+}
+
+export class Dealer implements IDealer {
+  constructor(
+    public id?: number,
+    public dealerName?: string,
+    public taxNumber?: string | null,
+    public postalAddress?: string | null,
+    public physicalAddress?: string | null,
+    public accountName?: string | null,
+    public accountNumber?: string | null,
+    public bankersName?: string | null,
+    public bankersBranch?: string | null,
+    public bankersSwiftCode?: string | null,
+    public paymentLabels?: IPaymentLabel[] | null,
+    public dealerGroup?: IDealer | null,
+    public paymentRequisitions?: IPaymentRequisition[] | null,
+    public signedPayments?: ISignedPayment[] | null,
+    public placeholders?: IPlaceholder[] | null
+  ) {}
+}
+
+export function getDealerIdentifier(dealer: IDealer): number | undefined {
+  return dealer.id;
+}
