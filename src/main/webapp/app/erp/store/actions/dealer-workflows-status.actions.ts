2aa390093ee8630b472c0a278d7cb3d399bbbc07
Edwin Njeru <mailnjeru@gmail.com>
2021-09-30 14:56:12 +0300

    Update category when going to payment-update from the dealers page

--- src/main/webapp/app/erp/store/actions/dealer-workflows-status.actions.ts
+++ src/main/webapp/app/erp/store/actions/dealer-workflows-status.actions.ts
@@ -3,15 +3,22 @@ import {IDealer} from '../../erp-pages/dealers/dealer/dealer.model';
 import {IPaymentCategory} from '../../erp-pages/payments/payment-category/payment-category.model';
 
 export const payDealerButtonClicked = createAction(
-  '[Dealers Payments Page] pay dealers button clicked',
-  props<{selectedDealer: IDealer}>()
-);
-
-export const paymentToDealerCompleted = createAction(
-  '[Payment Update form pay button] payment to dealer completed',
+  '[Dealer-category-service] pay dealers button clicked',
+  props<{
+    selectedDealer: IDealer,
+    paymentDealerCategory: IPaymentCategory
+  }>()
 );
 
 export const paymentDealerCategoryAcquired = createAction(
   '[Dealer-Category-Service] payment dealer category acquired',
   props<{paymentDealerCategory: IPaymentCategory}>()
+);
+
+export const paymentToDealerReset = createAction(
+  '[Payment-Update-Form] payment details with dealer reset',
+);
+
+export const paymentToDealerCompleted = createAction(
+  '[Payment-Update-Form pay button] payment details with dealer completed',
 );
--- src/main/webapp/app/erp/store/reducers/dealer-workflows-status.reducer.ts
+++ src/main/webapp/app/erp/store/reducers/dealer-workflows-status.reducer.ts
@@ -2,11 +2,11 @@ import {IDealer} from '../../erp-pages/dealers/dealer/dealer.model';
 import {Action, createReducer, on} from '@ngrx/store';
 import {initialState, State} from '../global-store.definition';
 import {
-  payDealerButtonClicked, paymentDealerCategoryAcquired,
-  paymentToDealerCompleted,
+  payDealerButtonClicked,
+  paymentToDealerCompleted, paymentToDealerReset,
 } from '../actions/dealer-workflows-status.actions';
-import {IPayment} from "../../erp-pages/payments/payment/payment.model";
-import {IPaymentCategory} from "../../erp-pages/payments/payment-category/payment-category.model";
+import {IPayment} from '../../erp-pages/payments/payment/payment.model';
+import {IPaymentCategory} from '../../erp-pages/payments/payment-category/payment-category.model';
 
 export const paymentToDealerWorkflowStateSelector = 'paymentToDealerWorkflows'
 
@@ -20,31 +20,33 @@ export interface DealerWorkflowState {
 const _dealerWorkflowStateReducer= createReducer(
   initialState,
 
-  on(payDealerButtonClicked, (state, {selectedDealer}) => ({
+  on(payDealerButtonClicked, (state, {selectedDealer, paymentDealerCategory}) => ({
      ...state,
     dealerWorkflowState: {
        ...state.dealerWorkflowState,
       selectedDealer,
+      paymentDealerCategory,
       weArePayingADealer: true
     }
    })),
+
+  on(paymentToDealerReset, (state) => ({
+    ...state,
+    dealerWorkflowState: {
+      ...state.dealerWorkflowState,
+      selectedDealer: {},
+      paymentDealerCategory: {},
+      weArePayingADealer: false
+    }
+  })),
 
   on(paymentToDealerCompleted, (state) => ({
     ...state,
     dealerWorkflowState: {
       ...state.dealerWorkflowState,
       selectedDealer: {},
+      paymentDealerCategory: {},
       weArePayingADealer: false
-    }
-  })),
-
-  on(paymentDealerCategoryAcquired, (state, {paymentDealerCategory}) => ({
-    ...state,
-    dealerWorkflowState: {
-      ...state.dealerWorkflowState,
-      selectedDealer: {},
-      paymentDealerCategory,
-      weArePayingADealer: true
     }
   })),
 );
