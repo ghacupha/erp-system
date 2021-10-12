c58e51284e54ed6b8552eef41dd6a38ac8f889a5
Edwin Njeru <mailnjeru@gmail.com>
2021-10-12 16:13:50 +0300

    Implement indexing for dealers

--- src/main/java/io/github/erp/erp/DealersIndexingService.java
+++ src/main/java/io/github/erp/erp/DealersIndexingService.java
@@ -5,22 +5,26 @@ import io.github.erp.repository.DealerRepository;
 import io.github.erp.repository.SignedPaymentRepository;
 import io.github.erp.repository.search.DealerSearchRepository;
 import io.github.erp.repository.search.SignedPaymentSearchRepository;
+import io.github.erp.service.DealerService;
+import io.github.erp.service.mapper.DealerMapper;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
+import org.springframework.data.domain.Pageable;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-// TODO Fix lazy init
-//@Service
-//@Transactional
+@Service
+@Transactional
 public class DealersIndexingService  extends AbtractStartUpIndexService implements ApplicationIndexingService, ApplicationListener<ApplicationReadyEvent> {
 
-    private final DealerRepository repository;
+    private final DealerService dealerService;
+    private final DealerMapper dealerMapper;
     private final DealerSearchRepository searchRepository;
 
-    public DealersIndexingService(DealerRepository repository, DealerSearchRepository searchRepository) {
-        this.repository = repository;
+    public DealersIndexingService(DealerService dealerService, DealerMapper dealerMapper, DealerSearchRepository searchRepository) {
+        this.dealerService = dealerService;
+        this.dealerMapper = dealerMapper;
         this.searchRepository = searchRepository;
     }
 
@@ -28,8 +32,9 @@ public class DealersIndexingService  extends AbtractStartUpIndexService implemen
     @Override
     public void index() {
         this.searchRepository.saveAll(
-            this.repository.findAll()
+            dealerService.findAll(Pageable.unpaged())
                 .stream()
+                .map(dealerMapper::toEntity)
                 .filter(entity -> !searchRepository.existsById(entity.getId()))
                 .collect(ImmutableList.toImmutableList()));
     }
