a1a1f5e573b799838384da9728dd63768d0b9b75
Edwin Njeru <mailnjeru@gmail.com>
2021-11-09 09:06:40 +0300

    Implement batch config for payment  model

---
+++ src/main/java/io/github/erp/internal/batch/payment/PaymentBatchConfigs.java
@@ -0,0 +1,175 @@
+package io.github.erp.internal.batch.payment;
+
+import com.google.common.collect.ImmutableList;
+import io.github.erp.domain.Payment;
+import io.github.erp.internal.framework.BatchService;
+import io.github.erp.internal.framework.FileUploadsProperties;
+import io.github.erp.internal.framework.Mapping;
+import io.github.erp.internal.framework.batch.BatchPersistentFileUploadService;
+import io.github.erp.internal.framework.batch.DataDeletionStep;
+import io.github.erp.internal.framework.batch.DeletionService;
+import io.github.erp.internal.framework.batch.EntityDeletionProcessor;
+import io.github.erp.internal.framework.batch.EntityItemsDeletionReader;
+import io.github.erp.internal.framework.batch.EntityItemsReader;
+import io.github.erp.internal.framework.batch.EntityListItemsWriter;
+import io.github.erp.internal.framework.batch.ReadFileStep;
+import io.github.erp.internal.framework.batch.SingleStepEntityJob;
+import io.github.erp.internal.framework.excel.ExcelFileDeserializer;
+import io.github.erp.internal.framework.model.FileUploadHasDataFile;
+import io.github.erp.internal.framework.service.DataFileContainer;
+import io.github.erp.internal.framework.service.DeletionUploadService;
+import io.github.erp.internal.model.PaymentBEO;
+import io.github.erp.internal.model.PaymentEVM;
+import io.github.erp.service.dto.PaymentDTO;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecutionListener;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class PaymentBatchConfigs {
+
+    private static final String PERSISTENCE_JOB_NAME = "paymentListPersistenceJob";
+    private static final String DELETION_JOB_NAME = "paymentListDeletionJob";
+    private static final String READ_FILE_STEP_NAME = "readPaymentListFromFile";
+    private static final String DELETION_STEP_NAME = "deletePaymentListFromFile";
+    private static final String DELETION_PROCESSOR_NAME = "paymentDeletionProcessor";
+    private static final String DELETION_WRITER_NAME = "paymentDeletionWriter";
+    private static final String DELETION_READER_NAME = "paymentDeletionReader";
+    private static final String PERSISTENCE_READER_NAME = "paymentListItemReader";
+    private static final String PERSISTENCE_PROCESSOR_NAME = "paymentListItemProcessor";
+    private static final String PERSISTENCE_WRITER_NAME = "paymentEntityListItemsWriter";
+
+    @SuppressWarnings("SpringElStaticFieldInjectionInspection")
+    @Value("#{jobParameters['fileId']}")
+    private static long fileId;
+
+    @SuppressWarnings("SpringElStaticFieldInjectionInspection")
+    @Value("#{jobParameters['messageToken']}")
+    private static String jobUploadToken;
+
+    @Autowired
+    private FileUploadsProperties fileUploadsProperties;
+
+    @Autowired
+    @Qualifier("persistenceJobListener")
+    private JobExecutionListener persistenceJobListener;
+
+    @Autowired
+    @Qualifier("deletionJobListener")
+    private JobExecutionListener deletionJobListener;
+
+    @Autowired
+    private ExcelFileDeserializer<PaymentEVM> paymentDeserializer;
+
+    @Autowired
+    private BatchService<PaymentDTO> batchService;
+
+    @Autowired
+    private DeletionService<Payment> paymentDeletionService;
+
+    @Autowired
+    private Mapping<PaymentEVM, PaymentDTO> mapping;
+
+    @Autowired
+    private JobBuilderFactory jobBuilderFactory;
+
+    @Autowired
+    private StepBuilderFactory stepBuilderFactory;
+
+    @Autowired
+    private BatchPersistentFileUploadService fileUploadService;
+
+    @Autowired
+    private DeletionUploadService<PaymentBEO> fileUploadDeletionService;
+
+    @Autowired
+    private DataFileContainer<FileUploadHasDataFile> dataFileContainer;
+
+    @Bean(PERSISTENCE_READER_NAME)
+    @JobScope
+    public EntityItemsReader<PaymentEVM> listItemReader(
+        @Value("#{jobParameters['fileId']}") long fileId
+    ) {
+        return new EntityItemsReader<>(paymentDeserializer, fileUploadService, fileId, fileUploadsProperties);
+    }
+
+    @Bean(PERSISTENCE_PROCESSOR_NAME)
+    @JobScope
+    public ItemProcessor<List<PaymentEVM>, List<PaymentDTO>> listItemsProcessor(
+        @Value("#{jobParameters['messageToken']}") String jobUploadToken
+    ) {
+        return evms ->
+            evms.stream().map(mapping::toValue2).peek(d -> d.setFileUploadToken(jobUploadToken)).collect(ImmutableList.toImmutableList());
+    }
+
+    @Bean(PERSISTENCE_WRITER_NAME)
+    @JobScope
+    public EntityListItemsWriter<PaymentDTO> listItemsWriter() {
+        return new EntityListItemsWriter<>(batchService);
+    }
+
+    @Bean(READ_FILE_STEP_NAME)
+    public Step readFile() {
+        return new ReadFileStep<>(
+            READ_FILE_STEP_NAME,
+            listItemReader(fileId),
+            listItemsProcessor(jobUploadToken),
+            listItemsWriter(),
+            stepBuilderFactory
+        );
+    }
+
+    @Bean(PERSISTENCE_JOB_NAME)
+    public Job persistenceJob() {
+        return new SingleStepEntityJob(PERSISTENCE_JOB_NAME, persistenceJobListener, readFile(), jobBuilderFactory);
+    }
+
+    // paymentDeletionJob
+    @Bean(DELETION_JOB_NAME)
+    public Job paymentDeletionJob() {
+        return new SingleStepEntityJob(DELETION_JOB_NAME, deletionJobListener, deleteEntityListFromFile(), jobBuilderFactory);
+    }
+
+    // deletePaymentListFromFile step
+    @Bean(DELETION_STEP_NAME)
+    public Step deleteEntityListFromFile() {
+        return new DataDeletionStep<>(
+            stepBuilderFactory,
+            DELETION_STEP_NAME,
+            deletionReader(fileId),
+            deletionProcessor(),
+            deletionWriter()
+        );
+    }
+
+
+
+    @Bean(DELETION_READER_NAME)
+    @JobScope
+    public ItemReader<List<Long>> deletionReader(@Value("#{jobParameters['fileId']}") long fileId) {
+        return new EntityItemsDeletionReader(fileId, fileUploadDeletionService, fileUploadsProperties, dataFileContainer);
+    }
+
+    @Bean(DELETION_PROCESSOR_NAME)
+    public ItemProcessor<List<Long>, List<Payment>> deletionProcessor() {
+        return new EntityDeletionProcessor<>(paymentDeletionService);
+    }
+
+    @Bean(DELETION_WRITER_NAME)
+    public ItemWriter<? super List<Payment>> deletionWriter() {
+        return deletables -> {};
+    }
+}
