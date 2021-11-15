f9f19dc26d60c72d61714cbc2649507a62661525
Edwin Njeru <mailnjeru@gmail.com>
2021-11-15 15:58:33 +0300

    Return implementing class from the items-processors and item-writers in the dealer-batch-configs

---
+++ src/main/java/io/github/erp/internal/batch/dealer/DealerPersistenceProcessor.java
@@ -0,0 +1,27 @@
+package io.github.erp.internal.batch.dealer;
+
+import com.google.common.collect.ImmutableList;
+import io.github.erp.internal.framework.Mapping;
+import io.github.erp.internal.model.DealerEVM;
+import io.github.erp.service.dto.DealerDTO;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.lang.NonNull;
+
+import java.util.List;
+
+public class DealerPersistenceProcessor implements ItemProcessor<List<DealerEVM>, List<DealerDTO>> {
+
+    private final Mapping<DealerEVM, DealerDTO> mapping;
+    private final String uploadToken;
+
+    @org.jetbrains.annotations.Contract(pure = true)
+    public DealerPersistenceProcessor(Mapping<DealerEVM, DealerDTO> mapping, String uploadToken) {
+        this.mapping = mapping;
+        this.uploadToken = uploadToken;
+    }
+
+    @Override
+    public List<DealerDTO> process(@NonNull List<DealerEVM> evms) throws Exception {
+        return evms.stream().map(mapping::toValue2).peek(d -> d.setFileUploadToken(uploadToken)).collect(ImmutableList.toImmutableList());
+    }
+}
