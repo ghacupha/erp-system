543fd6ef90801e968cf4b0e593fa1c11de36c206
Edwin Njeru <mailnjeru@gmail.com>
2021-11-15 18:35:32 +0300

    Implement the item-processors in the batches as implementations of the list processor interface

--- src/main/java/io/github/erp/internal/batch/signedPayment/SignedPaymentPersistenceProcessor.java
+++ src/main/java/io/github/erp/internal/batch/signedPayment/SignedPaymentPersistenceProcessor.java
@@ -9,6 +9,9 @@ import org.springframework.lang.NonNull;
 
 import java.util.List;
 
+/**
+ * List processor implementation that also adds additional data to the output, i.e. the file-processing-token
+ */
 public class SignedPaymentPersistenceProcessor implements ListProcessor<SignedPaymentEVM, SignedPaymentDTO> {
 
 
