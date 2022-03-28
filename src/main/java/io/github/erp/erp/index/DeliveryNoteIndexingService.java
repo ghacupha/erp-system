7e5f34fb54efc93eb7ea882c4b9f5fcadf70bbed
Edwin Njeru <mailnjeru@gmail.com>
2022-03-28 17:11:25 +0300

    Creates business-stamps and delivery-note indexing service

--- src/main/java/io/github/erp/erp/index/DeliveryNoteIndexingService.java
+++ src/main/java/io/github/erp/erp/index/DeliveryNoteIndexingService.java
@@ -1,9 +1,9 @@
 package io.github.erp.erp.index;
 
 import com.google.common.collect.ImmutableList;
-import io.github.erp.repository.search.InvoiceSearchRepository;
-import io.github.erp.service.InvoiceService;
-import io.github.erp.service.mapper.InvoiceMapper;
+import io.github.erp.repository.search.DeliveryNoteSearchRepository;
+import io.github.erp.service.DeliveryNoteService;
+import io.github.erp.service.mapper.DeliveryNoteMapper;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.data.domain.Pageable;
@@ -11,15 +11,15 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-// @Service
-// @Transactional
-public class InvoiceIndexingService extends AbtractStartUpIndexService implements ApplicationIndexingService, ApplicationListener<ApplicationReadyEvent> {
+@Service
+@Transactional
+public class DeliveryNoteIndexingService extends AbtractStartUpIndexService implements ApplicationIndexingService, ApplicationListener<ApplicationReadyEvent> {
 
-    private final InvoiceMapper mapper;
-    private final InvoiceService service;
-    private final InvoiceSearchRepository searchRepository;
+    private final DeliveryNoteMapper mapper;
+    private final DeliveryNoteService service;
+    private final DeliveryNoteSearchRepository searchRepository;
 
-    public InvoiceIndexingService(InvoiceMapper mapper, InvoiceService service, InvoiceSearchRepository searchRepository) {
+    public DeliveryNoteIndexingService(DeliveryNoteMapper mapper, DeliveryNoteService service, DeliveryNoteSearchRepository searchRepository) {
         this.mapper = mapper;
         this.service = service;
         this.searchRepository = searchRepository;
