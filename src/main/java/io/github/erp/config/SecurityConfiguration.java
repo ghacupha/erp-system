5c90de7011818b40fa4ac93ef4395facf4d87dce
Edwin Njeru <mailnjeru@gmail.com>
2022-05-03 14:27:39 +0300

    Migrates the prepayment-amount col on the prepayment-account entity

--- src/main/java/io/github/erp/config/SecurityConfiguration.java
+++ src/main/java/io/github/erp/config/SecurityConfiguration.java
@@ -85,7 +85,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .antMatchers("/api/account/reset-password/init").permitAll()
             .antMatchers("/api/account/reset-password/finish").permitAll()
             .antMatchers("/api/admin/**").hasAuthority(AuthoritiesConstants.ADMIN)
-            .antMatchers("/api/accounts/**").hasAnyAuthority(AuthoritiesConstants.FIXED_ASSETS_USER, AuthoritiesConstants.BOOK_KEEPING)
+            .antMatchers("/api/accounts/**").hasAnyAuthority(
+                AuthoritiesConstants.FIXED_ASSETS_USER,
+                AuthoritiesConstants.BOOK_KEEPING)
             .antMatchers("/api/fixed-asset/**").hasAuthority(AuthoritiesConstants.FIXED_ASSETS_USER)
             .antMatchers("/api/files/**").hasAuthority(AuthoritiesConstants.DBA)
             .antMatchers("/api/dev/**").hasAuthority(AuthoritiesConstants.DEV)
@@ -97,7 +99,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .antMatchers("/api/taxes/**").hasAuthority(AuthoritiesConstants.TAX_MODULE_USER)
             .antMatchers("/api/granular-data/**").hasAnyAuthority(
                 AuthoritiesConstants.GRANULAR_REPORTS_USER,
-                AuthoritiesConstants.PREPAYMENTS_MODULE_USER, 
+                AuthoritiesConstants.PREPAYMENTS_MODULE_USER,
                 AuthoritiesConstants.FIXED_ASSETS_USER)
             .antMatchers("/api/**").authenticated()
             .antMatchers("/websocket/**").authenticated()
