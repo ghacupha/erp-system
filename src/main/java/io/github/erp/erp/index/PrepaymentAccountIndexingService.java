76cc734f69481af4b63ea1b0afc95a70db044b13
Edwin Njeru <mailnjeru@gmail.com>
2022-05-16 16:18:21 +0300

    Create index for prepayments and assets

---
+++ src/main/java/io/github/erp/erp/index/PrepaymentAccountIndexingService.java
@@ -0,0 +1,46 @@
+package io.github.erp.erp.index;
+
+import com.google.common.collect.ImmutableList;
+import io.github.erp.repository.search.AssetCategorySearchRepository;
+import io.github.erp.service.AssetCategoryService;
+import io.github.erp.service.mapper.AssetCategoryMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.data.domain.Pageable;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class PrepaymentAccountIndexingService extends AbtractStartUpIndexService implements ApplicationIndexingService, ApplicationListener<ApplicationReadyEvent> {
+    private static final String TAG = "PrepaymentAccountIndexi";
+    private static final Logger log = LoggerFactory.getLogger(TAG);
+
+    private final AssetCategoryMapper mapper;
+    private final AssetCategoryService service;
+    private final AssetCategorySearchRepository searchRepository;
+
+    public PrepaymentAccountIndexingService(AssetCategoryMapper mapper, AssetCategoryService service, AssetCategorySearchRepository searchRepository) {
+        this.mapper = mapper;
+        this.service = service;
+        this.searchRepository = searchRepository;
+    }
+
+    @Async
+    @Override
+    public void index() {
+        log.info("Initiating {} build sequence", TAG);
+        long startup = System.currentTimeMillis();
+        this.searchRepository.saveAll(
+            service.findAll(Pageable.unpaged())
+                .stream()
+                .map(mapper::toEntity)
+                .filter(entity -> !searchRepository.existsById(entity.getId()))
+                .collect(ImmutableList.toImmutableList()));
+        log.info("{} initiated and ready for queries. Index build has taken {} milliseconds", TAG, System.currentTimeMillis() - startup);
+    }
+
+}
