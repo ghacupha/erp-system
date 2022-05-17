df2d6c3ffc81bdeb6e802c69357aa3c2f09813ab
Edwin Njeru <mailnjeru@gmail.com>
2022-05-17 10:13:23 +0300

    Adds license headers

--- src/main/docker/jib/entrypoint.sh
+++ src/main/docker/jib/entrypoint.sh
@@ -1,4 +1,22 @@
 #!/bin/sh
+#
+# Erp System - Mark I Ver 1 (Artaxerxes)
+# Copyright © 2021 Edwin Njeru (mailnjeru@gmail.com)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
 
 echo "The application will start in ${JHIPSTER_SLEEP}s..." && sleep ${JHIPSTER_SLEEP}
 exec java ${JAVA_OPTS} -noverify -XX:+AlwaysPreTouch -Djava.security.egd=file:/dev/./urandom -cp /app/resources/:/app/classes/:/app/libs/* "io.github.erp.ErpSystemApp"  "$@"
--- src/main/java/io/github/erp/internal/framework/service/DeletionUploadService.java
+++ src/main/java/io/github/erp/internal/framework/service/DeletionUploadService.java
@@ -1,7 +1,7 @@
 package io.github.erp.internal.framework.service;
 
 /*-
- * ERP System - ERP data management platform
+ * Erp System - Mark I Ver 1 (Artaxerxes)
  * Copyright © 2021 Edwin Njeru (mailnjeru@gmail.com)
  *
  * This program is free software: you can redistribute it and/or modify
@@ -17,7 +17,6 @@ package io.github.erp.internal.framework.service;
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-
 import java.util.List;
 import java.util.Optional;
 
