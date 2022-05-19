4216c49ca4eb779db05f9e98187ab1462864cbab
Edwin Njeru <mailnjeru@gmail.com>
2022-05-19 14:02:46 +0300

    User file-input-stream to access report-file

--- src/main/java/io/github/erp/internal/report/SimpleJasperReportsService.java
+++ src/main/java/io/github/erp/internal/report/SimpleJasperReportsService.java
@@ -2,6 +2,7 @@ package io.github.erp.internal.report;
 
 import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.JasperReport;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
@@ -9,6 +10,9 @@ import java.util.Map;
 
 @Service
 public class SimpleJasperReportsService {
+
+    @Value("${jhipster.clientApp.name}")
+    private String applicationName;
 
     private final SimpleJasperReportCompiler compiler;
     private SimpleJasperReportFiller simpleReportFiller;
@@ -22,7 +26,7 @@ public class SimpleJasperReportsService {
 
     public void generateReport() {
 
-        JasperReport compiledReport = compiler.compileReport("templates/reports/Simple_Blue.jrxml");
+        JasperReport compiledReport = compiler.compileReport("generated-reports/Simple_Blue.jrxml");
 
         Map<String, Object> parameters = new HashMap<>();
         parameters.put("title", "Dealers Report Example");
@@ -31,7 +35,7 @@ public class SimpleJasperReportsService {
 
         simpleExporter.setJasperPrint(print);
 
-        simpleExporter.exportToPdf("generated-reports/employeeReport.pdf", "baeldung");
+        simpleExporter.exportToPdf("generated-reports/employeeReport.pdf", applicationName);
         simpleExporter.exportToXlsx("generated-reports/employeeReport.xlsx", "Employee Data");
         simpleExporter.exportToCsv("generated-reports/employeeReport.csv");
         simpleExporter.exportToHtml("generated-reports/employeeReport.html");
