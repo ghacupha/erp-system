3652461f88778786abbd62ff14dada7501178394
Edwin Njeru <mailnjeru@gmail.com>
2022-05-19 14:02:46 +0300

    User file-input-stream to access report-file

--- src/main/java/io/github/erp/internal/report/SimpleJasperReportCompiler.java
+++ src/main/java/io/github/erp/internal/report/SimpleJasperReportCompiler.java
@@ -1,10 +1,14 @@
 package io.github.erp.internal.report;
 
 import lombok.SneakyThrows;
+import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JasperCompileManager;
 import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.util.JRSaver;
 import org.springframework.stereotype.Service;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.InputStream;
 
 @Service
@@ -12,14 +16,17 @@ public class SimpleJasperReportCompiler {
 
     @SneakyThrows
     public JasperReport compileReport(String reportFileName) {
-        InputStream reportStream = getClass().getResourceAsStream("/".concat(reportFileName));
+        // InputStream reportStream = getClass().getResourceAsStream(reportFileName);
+        File reportFile = new File(reportFileName);
+
+        InputStream reportStream = new FileInputStream(reportFile);
         JasperReport jasperReport = JasperCompileManager.compileReport(reportStream);
 
-        return saveReport(jasperReport);
+        return saveReport(jasperReport, reportFileName);
     }
 
-    private JasperReport saveReport(JasperReport jasperReport) {
-        // TODO JRSaver.saveObject(jasperReport, reportFileName.replace(".jrxml", ".jasper"));
+    private JasperReport saveReport(JasperReport jasperReport, String reportFileName) throws JRException {
+        JRSaver.saveObject(jasperReport, reportFileName.replace(".jrxml", ".jasper"));
 
         return jasperReport;
     }
