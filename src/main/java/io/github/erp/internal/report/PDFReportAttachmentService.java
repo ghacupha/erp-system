04233b3889e67724f741bbe5f0547ffe16b70b20
Edwin Njeru <mailnjeru@gmail.com>
2022-05-26 12:51:55 +0300

    Add a filename that is equivalent to the report-id

--- src/main/java/io/github/erp/internal/report/PDFReportAttachmentService.java
+++ src/main/java/io/github/erp/internal/report/PDFReportAttachmentService.java
@@ -3,10 +3,9 @@ package io.github.erp.internal.report;
 import io.github.erp.internal.files.FileStorageService;
 import io.github.erp.service.dto.PdfReportRequisitionDTO;
 import lombok.SneakyThrows;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.util.Optional;
 
 @Service
@@ -22,11 +21,16 @@ public class PDFReportAttachmentService implements ReportAttachmentService<Optio
     @Override
     public Optional<PdfReportRequisitionDTO> attachReport(Optional<PdfReportRequisitionDTO> one) {
 
-        byte[] reportResource = fileStorageService.load("employeeReport.pdf").getInputStream().readAllBytes();
-
         PdfReportRequisitionDTO attached = null;
         one.ifPresent(
-            pdfReportRequisitionDTO -> pdfReportRequisitionDTO.setReportAttachment(reportResource)
+            dto -> {
+                byte[] reportResource = new byte[0];
+                try {
+                    reportResource = fileStorageService.load(dto.getReportId().toString().concat(".pdf")).getInputStream().readAllBytes();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                dto.setReportAttachment(reportResource);}
         );
 
         return one;
