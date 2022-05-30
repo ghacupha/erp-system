b516c816f6c5940a155f2c6d85fa702e2d636088
Edwin Njeru <mailnjeru@gmail.com>
2022-05-30 17:31:19 +0300

    Configure the reports dir using external settings

--- src/main/java/io/github/erp/internal/report/ReportsProperties.java
+++ src/main/java/io/github/erp/internal/report/ReportsProperties.java
@@ -9,4 +9,14 @@ import org.springframework.context.annotation.PropertySource;
 @ConfigurationProperties(prefix = "erp")
 @PropertySource(value = "classpath:config/erpConfigs.yml", factory = AppPropertyFactory.class)
 public class ReportsProperties {
+
+    private String reportsDirectory;
+
+    public String getReportsDirectory() {
+        return reportsDirectory;
+    }
+
+    public void setReportsDirectory(String reportsDirectory) {
+        this.reportsDirectory = reportsDirectory;
+    }
 }
