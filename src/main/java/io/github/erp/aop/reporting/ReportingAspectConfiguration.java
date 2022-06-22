ffa448c686171e2a36659c9b7ee1f12a4a9802ec
Edwin Njeru <mailnjeru@gmail.com>
2022-06-22 14:43:39 +0300

    Implement xlsx-report-requisition intercept

--- src/main/java/io/github/erp/aop/reporting/ReportingAspectConfiguration.java
+++ src/main/java/io/github/erp/aop/reporting/ReportingAspectConfiguration.java
@@ -2,8 +2,12 @@ package io.github.erp.aop.reporting;
 
 import io.github.erp.internal.report.ReportAssemblyService;
 import io.github.erp.service.PdfReportRequisitionService;
+import io.github.erp.service.ReportContentTypeService;
+import io.github.erp.service.ReportRequisitionService;
+import io.github.erp.service.XlsxReportRequisitionService;
 import io.github.erp.service.dto.PdfReportRequisitionDTO;
 import io.github.erp.service.dto.ReportRequisitionDTO;
+import io.github.erp.service.dto.XlsxReportRequisitionDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,7 +16,7 @@ import org.springframework.context.annotation.Configuration;
 public class ReportingAspectConfiguration {
 
     @Autowired
-    private ReportAssemblyService<ReportRequisitionDTO> reportRequisitionService;
+    private ReportAssemblyService<ReportRequisitionDTO> reportAssemblyService;
 
     @Autowired
     private PdfReportRequisitionService pdfReportRequisitionService;
@@ -20,15 +24,33 @@ public class ReportingAspectConfiguration {
     @Autowired
     private ReportAssemblyService<PdfReportRequisitionDTO> pdfReportAssemblyService;
 
+    @Autowired
+    private XlsxReportRequisitionService xlsxReportRequisitionService;
+
+    @Autowired
+    private ReportAssemblyService<XlsxReportRequisitionDTO> xlsxReportAssemblyService;
+
+    @Autowired
+    private ReportContentTypeService reportContentTypeService;
+
+    @Autowired
+    private ReportRequisitionService reportRequisitionService;
+
     @Bean
     public ReportRequisitionInterceptor reportRequisitionInterceptor() {
 
-        return new ReportRequisitionInterceptor(reportRequisitionService);
+        return new ReportRequisitionInterceptor(reportContentTypeService, reportRequisitionService, reportAssemblyService);
     }
 
     @Bean
     public PDFReportRequisitionInterceptor pdfReportRequisitionInterceptor() {
 
         return new PDFReportRequisitionInterceptor(pdfReportAssemblyService, pdfReportRequisitionService);
+    }
+
+    @Bean
+    public XLSXReportRequisitionInterceptor xlsxReportRequisitionInterceptor() {
+
+        return new XLSXReportRequisitionInterceptor(xlsxReportAssemblyService, xlsxReportRequisitionService);
     }
 }
