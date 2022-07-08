31febd2565ac407aa2c5ddc78bfa24fc7d885c57
Edwin Njeru <mailnjeru@gmail.com>
2022-07-08 18:08:37 +0300

    Minor doc fixes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,12 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'ERP System'
+copyright = '2022, Edwin Njeru'
+author = 'Edwin Njeru'
 
-release = '0.1'
-version = '0.1.0'
+release = '0.0.1-SNAPSHOT'
+version = '0.0.1-SNAPSHOT'
 
 # -- General configuration
 
