6243a0a75f36bd96d525e125c0c88ed12b658a9a
Edwin Njeru <mailnjeru@gmail.com>
2022-11-10 16:03:30 +0300

    Releasing version 0.1.4-SNAPSHOT, implementing Mark III no 4

--- src/main/java/io/github/erp/web/websocket/dto/ActivityDTO.java
+++ src/main/java/io/github/erp/web/websocket/dto/ActivityDTO.java
@@ -1,7 +1,7 @@
 package io.github.erp.web.websocket.dto;
 
 /*-
- * Erp System - Mark III No 3 (Caleb Series) Server ver 0.1.3-SNAPSHOT
+ * Erp System - Mark III No 4 (Caleb Series) Server ver 0.1.4-SNAPSHOT
  * Copyright © 2021 - 2022 Edwin Njeru (mailnjeru@gmail.com)
  *
  * This program is free software: you can redistribute it and/or modify
