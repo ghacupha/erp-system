6fbf8f33da6c1b5301e0cd0d896ff7dda36ee71a
Edwin Njeru <mailnjeru@gmail.com>
2022-11-10 16:03:30 +0300

    Releasing version 0.1.4-SNAPSHOT, implementing Mark III no 4

--- src/main/java/io/github/erp/service/WorkInProgressTransferQueryService.java
+++ src/main/java/io/github/erp/service/WorkInProgressTransferQueryService.java
@@ -1,5 +1,22 @@
 package io.github.erp.service;
 
+/*-
+ * Erp System - Mark III No 4 (Caleb Series) Server ver 0.1.4-SNAPSHOT
+ * Copyright © 2021 - 2022 Edwin Njeru (mailnjeru@gmail.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 import io.github.erp.domain.*; // for static metamodels
 import io.github.erp.domain.WorkInProgressTransfer;
 import io.github.erp.repository.WorkInProgressTransferRepository;
