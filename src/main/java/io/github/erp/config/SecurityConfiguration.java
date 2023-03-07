e2e35548ab2afba495b7c8db267b5d08a406c717
Edwin Njeru <mailnjeru@gmail.com>
2023-03-07 11:42:31 +0300

    Configure security to allow api logs on console

--- src/main/java/io/github/erp/config/SecurityConfiguration.java
+++ src/main/java/io/github/erp/config/SecurityConfiguration.java
@@ -97,6 +97,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
         .and()
             .authorizeRequests()
+            .antMatchers("/api/logs").permitAll()
             .antMatchers("/api/authenticate").permitAll()
             .antMatchers("/api/register").permitAll()
             .antMatchers("/api/activate").permitAll()
