44f565fa501bc13e4f7c26fe94778b96c52b5c0f
Edwin Njeru <mailnjeru@gmail.com>
2023-03-20 20:45:09 +0300

    Remove user tests to prevent IT tests dirtifying the context

--- src/test/java/io/github/erp/erp/resources/ApplicationUserResourceIT.java
+++ src/test/java/io/github/erp/erp/resources/ApplicationUserResourceIT.java
@@ -25,7 +25,7 @@ import io.github.erp.service.ApplicationUserService;
 import io.github.erp.service.dto.ApplicationUserDTO;
 import io.github.erp.service.mapper.ApplicationUserMapper;
 import io.github.erp.web.rest.TestUtil;
-import io.github.erp.web.rest.userManagement.UserResourceIT;
+import io.github.erp.web.rest.UserResourceIT;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
