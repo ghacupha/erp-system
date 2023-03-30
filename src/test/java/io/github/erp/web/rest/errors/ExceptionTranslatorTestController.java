76fd3f64d4ad364b71a1af163b2c0376987d63fb
Edwin Njeru <mailnjeru@gmail.com>
2023-03-30 13:07:50 +0300

    Releaseing version 1.0.5-SNAPSHOT

--- src/test/java/io/github/erp/web/rest/errors/ExceptionTranslatorTestController.java
+++ src/test/java/io/github/erp/web/rest/errors/ExceptionTranslatorTestController.java
@@ -1,7 +1,7 @@
 package io.github.erp.web.rest.errors;
 
 /*-
- * Erp System - Mark III No 12 (Caleb Series) Server ver 1.0.2-SNAPSHOT
+ * Erp System - Mark III No 12 (Caleb Series) Server ver 1.0.5-SNAPSHOT
  * Copyright © 2021 - 2023 Edwin Njeru (mailnjeru@gmail.com)
  *
  * This program is free software: you can redistribute it and/or modify
