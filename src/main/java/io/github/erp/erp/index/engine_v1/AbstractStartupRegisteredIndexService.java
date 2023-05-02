2eb33ec8345511978a7c06917436e045c9ade16d
Edwin Njeru <mailnjeru@gmail.com>
2023-05-02 15:49:31 +0300

    Setup framework for registration and user-triggered index update

--- src/main/java/io/github/erp/erp/index/engine_v1/AbstractStartupRegisteredIndexService.java
+++ src/main/java/io/github/erp/erp/index/engine_v1/AbstractStartupRegisteredIndexService.java
@@ -7,13 +7,17 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 
+/**
+ * This objects updates registration of index services on startup. The registered services can then be invoked
+ * at runtime to recreate or update the existing index
+ */
 public abstract class AbstractStartupRegisteredIndexService implements ControllerIndexingService, ApplicationIndexingService, ApplicationListener<ApplicationReadyEvent> {
 
     private static final Logger log = LoggerFactory.getLogger("IndexingServiceRegistrationSequence");
 
     @Override
     public void onApplicationEvent(@NotNull ApplicationReadyEvent applicationReadyEvent) {
-        log.info("Index build initiated. Standby for next sequence");
+        log.info("Index Service registration initiated. Standby for next sequence");
 
         register();
     }
--- src/main/java/io/github/erp/erp/index/engine_v1/AsynchronousIndexingService.java
+++ src/main/java/io/github/erp/erp/index/engine_v1/AsynchronousIndexingService.java
@@ -2,6 +2,9 @@ package io.github.erp.erp.index.engine_v1;
 
 import org.springframework.stereotype.Service;
 
+/**
+ * Once this service is triggered it runs the indexing method on all registered services
+ */
 @Service("AsynchronousIndexingService")
 public class AsynchronousIndexingService {
 
