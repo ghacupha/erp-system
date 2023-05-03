180e8c76df4f320301eca7158eae686517db9b2a
Edwin Njeru <mailnjeru@gmail.com>
2023-05-03 18:59:39 +0300

    Releasing milestpne 14

--- src/main/java/io/github/erp/erp/events/EventListener.java
+++ src/main/java/io/github/erp/erp/events/EventListener.java
@@ -1,7 +1,7 @@
 package io.github.erp.erp.events;
 
 /*-
- * Erp System - Mark III No 13 (Caleb Series) Server ver 1.1.3-SNAPSHOT
+ * Erp System - Mark III No 14 (Caleb Series) Server ver 1.1.4-SNAPSHOT
  * Copyright © 2021 - 2023 Edwin Njeru (mailnjeru@gmail.com)
  *
  * This program is free software: you can redistribute it and/or modify
@@ -17,7 +17,6 @@ package io.github.erp.erp.events;
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-
 /**
  * Asynchronous listener
  */
--- src/main/java/io/github/erp/erp/index/LeaseLiabilityScheduleItemIndexingService.java
+++ src/main/java/io/github/erp/erp/index/LeaseLiabilityScheduleItemIndexingService.java
@@ -1,7 +1,7 @@
 package io.github.erp.erp.index;
 
 /*-
- * Erp System - Mark III No 13 (Caleb Series) Server ver 1.1.3-SNAPSHOT
+ * Erp System - Mark III No 14 (Caleb Series) Server ver 1.1.4-SNAPSHOT
  * Copyright © 2021 - 2023 Edwin Njeru (mailnjeru@gmail.com)
  *
  * This program is free software: you can redistribute it and/or modify
@@ -17,7 +17,6 @@ package io.github.erp.erp.index;
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-
 import com.google.common.collect.ImmutableList;
 import io.github.erp.erp.index.engine_v1.AbstractStartupRegisteredIndexService;
 import io.github.erp.erp.index.engine_v1.IndexingServiceChainSingleton;
