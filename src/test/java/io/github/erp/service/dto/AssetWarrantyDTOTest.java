2a5e7ebd4e66c4c7c9573304199a533d951e0b33
Edwin Njeru <mailnjeru@gmail.com>
2023-05-08 15:45:11 +0300

    Implement asset-warranty entity

---
+++ src/test/java/io/github/erp/service/dto/AssetWarrantyDTOTest.java
@@ -0,0 +1,42 @@
+package io.github.erp.service.dto;
+
+/*-
+ * Erp System - Mark III No 15 (Caleb Series) Server ver 1.2.0-SNAPSHOT
+ * Copyright © 2021 - 2023 Edwin Njeru (mailnjeru@gmail.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.github.erp.web.rest.TestUtil;
+import org.junit.jupiter.api.Test;
+
+class AssetWarrantyDTOTest {
+
+    @Test
+    void dtoEqualsVerifier() throws Exception {
+        TestUtil.equalsVerifier(AssetWarrantyDTO.class);
+        AssetWarrantyDTO assetWarrantyDTO1 = new AssetWarrantyDTO();
+        assetWarrantyDTO1.setId(1L);
+        AssetWarrantyDTO assetWarrantyDTO2 = new AssetWarrantyDTO();
+        assertThat(assetWarrantyDTO1).isNotEqualTo(assetWarrantyDTO2);
+        assetWarrantyDTO2.setId(assetWarrantyDTO1.getId());
+        assertThat(assetWarrantyDTO1).isEqualTo(assetWarrantyDTO2);
+        assetWarrantyDTO2.setId(2L);
+        assertThat(assetWarrantyDTO1).isNotEqualTo(assetWarrantyDTO2);
+        assetWarrantyDTO1.setId(null);
+        assertThat(assetWarrantyDTO1).isNotEqualTo(assetWarrantyDTO2);
+    }
+}
