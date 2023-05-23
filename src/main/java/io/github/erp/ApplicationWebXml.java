ba2d1f199bcd3e6db9bb20254ae8ca6515e4e4b1
Edwin Njeru <mailnjeru@gmail.com>
2023-05-23 07:15:36 +0300

    Releasing v1.2.3

--- src/main/java/io/github/erp/ApplicationWebXml.java
+++ src/main/java/io/github/erp/ApplicationWebXml.java
@@ -1,7 +1,7 @@
 package io.github.erp;
 
 /*-
- * Erp System - Mark III No 15 (Caleb Series) Server ver 1.2.2
+ * Erp System - Mark III No 15 (Caleb Series) Server ver 1.2.3
  * Copyright © 2021 - 2023 Edwin Njeru (mailnjeru@gmail.com)
  *
  * This program is free software: you can redistribute it and/or modify
