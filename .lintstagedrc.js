117685476b9873db7d22f31cf181c41ac8717a8a
Edwin Njeru <mailnjeru@gmail.com>
2023-05-23 07:15:36 +0300

    Releasing v1.2.3

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,5 +1,5 @@
 /*
- * Erp System - Mark III No 15 (Caleb Series) Server ver 1.2.2
+ * Erp System - Mark III No 15 (Caleb Series) Server ver 1.2.3
  * Copyright © 2021 - 2023 Edwin Njeru (mailnjeru@gmail.com)
  *
  * This program is free software: you can redistribute it and/or modify
