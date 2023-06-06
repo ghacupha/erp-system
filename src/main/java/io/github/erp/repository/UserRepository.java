47cfe617758c0e5ae96d782b458ce82486ff54e1
Edwin Njeru <mailnjeru@gmail.com>
2023-06-06 13:59:55 +0300

    Release version 1.2.5

--- src/main/java/io/github/erp/repository/UserRepository.java
+++ src/main/java/io/github/erp/repository/UserRepository.java
@@ -1,7 +1,7 @@
 package io.github.erp.repository;
 
 /*-
- * Erp System - Mark III No 15 (Caleb Series) Server ver 1.2.4
+ * Erp System - Mark III No 15 (Caleb Series) Server ver 1.2.5
  * Copyright © 2021 - 2023 Edwin Njeru (mailnjeru@gmail.com)
  *
  * This program is free software: you can redistribute it and/or modify
