ab1a962e0a92fd9e29646e74163a3839e182e586
Edwin Njeru <mailnjeru@gmail.com>
2023-06-12 15:57:51 +0300

    Releasing version 1.2.7

--- src/test/java/io/github/erp/web/rest/UserJWTControllerIT.java
+++ src/test/java/io/github/erp/web/rest/UserJWTControllerIT.java
@@ -197,7 +197,7 @@
 package io.github.erp.web.rest;
 
 /*-
- * Erp System - Mark III No 15 (Caleb Series) Server ver 1.2.6
+ * Erp System - Mark III No 16 (Caleb Series) Server ver 1.2.7
  * Copyright © 2021 - 2023 Edwin Njeru (mailnjeru@gmail.com)
  *
  * This program is free software: you can redistribute it and/or modify
