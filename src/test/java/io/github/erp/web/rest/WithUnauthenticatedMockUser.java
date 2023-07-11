8161599296b81153e4893b6f1370c9de332db28f
Edwin Njeru <mailnjeru@gmail.com>
2023-07-11 18:05:16 +0300

    Install kafka into the system

---
+++ src/test/java/io/github/erp/web/rest/WithUnauthenticatedMockUser.java
@@ -0,0 +1,42 @@
+package io.github.erp.web.rest;
+
+/*-
+ * Erp System - Mark IV No 1 (Ehud Series) Server ver 1.3.1
+ * Copyright © 2021 - 2023 Edwin Njeru (mailnjeru@gmail.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.test.context.support.WithSecurityContext;
+import org.springframework.security.test.context.support.WithSecurityContextFactory;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ ElementType.METHOD, ElementType.TYPE })
+@Retention(RetentionPolicy.RUNTIME)
+@WithSecurityContext(factory = WithUnauthenticatedMockUser.Factory.class)
+public @interface WithUnauthenticatedMockUser {
+    class Factory implements WithSecurityContextFactory<WithUnauthenticatedMockUser> {
+
+        @Override
+        public SecurityContext createSecurityContext(WithUnauthenticatedMockUser annotation) {
+            return SecurityContextHolder.createEmptyContext();
+        }
+    }
+}
