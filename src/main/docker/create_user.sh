bcb2e92a41d692c0d89395292e6815844ce0bc15
Edwin Njeru <mailnjeru@gmail.com>
2023-08-04 08:33:31 +0300

    Clean up create user script

--- src/main/docker/create_user.sh
+++ src/main/docker/create_user.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-#adduser --disabled-password nonrootuser
-
-
-If the --disabled-password option is not supported in the container's environment, you can use other options that are generally supported in most Linux environments. One common approach is to create the user without specifying a password and then later assign a random password or no password at all.
-
-Here's an example of how you could modify the create_user.sh script:
-
-bash
-Copy code
-#!/bin/bash
-
 # Create a non-root user inside the container
 adduser nonrootuser
 
