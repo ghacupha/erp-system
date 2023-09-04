fc449c84fd285a73fe23730f937a89818d458da0
Edwin Njeru <mailnjeru@gmail.com>
2023-09-04 10:07:25 +0300

    Deploy version 1.4.0

--- src/main/java/io/github/erp/internal/framework/util/TokenGenerator.java
+++ src/main/java/io/github/erp/internal/framework/util/TokenGenerator.java
@@ -1,7 +1,7 @@
 package io.github.erp.internal.framework.util;
 
 /*-
- * Erp System - Mark IV No 5 (Ehud Series) Server ver 1.3.6
+ * Erp System - Mark IV No 6 (Ehud Series) Server ver 1.4.0
  * Copyright © 2021 - 2023 Edwin Njeru (mailnjeru@gmail.com)
  *
  * This program is free software: you can redistribute it and/or modify
