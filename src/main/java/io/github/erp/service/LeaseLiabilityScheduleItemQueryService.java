7aa83c6c70b998a8b9f6ed27b2c355d27bcacf01
Edwin Njeru <mailnjeru@gmail.com>
2023-11-10 09:33:07 +0300

    Implement  summary reports

--- src/main/java/io/github/erp/service/LeaseLiabilityScheduleItemQueryService.java
+++ src/main/java/io/github/erp/service/LeaseLiabilityScheduleItemQueryService.java
@@ -1,7 +1,7 @@
 package io.github.erp.service;
 
 /*-
- * Erp System - Mark VII No 1 (Gideon Series) Server ver 1.5.5
+ * Erp System - Mark VII No 2 (Gideon Series) Server ver 1.5.6
  * Copyright © 2021 - 2023 Edwin Njeru (mailnjeru@gmail.com)
  *
  * This program is free software: you can redistribute it and/or modify
@@ -17,7 +17,6 @@ package io.github.erp.service;
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-
 import io.github.erp.domain.*; // for static metamodels
 import io.github.erp.domain.LeaseLiabilityScheduleItem;
 import io.github.erp.repository.LeaseLiabilityScheduleItemRepository;
--- src/main/java/io/github/erp/web/rest/FiscalQuarterResource.java
+++ src/main/java/io/github/erp/web/rest/FiscalQuarterResource.java
@@ -1,7 +1,7 @@
 package io.github.erp.web.rest;
 
 /*-
- * Erp System - Mark VII No 1 (Gideon Series) Server ver 1.5.5
+ * Erp System - Mark VII No 2 (Gideon Series) Server ver 1.5.6
  * Copyright © 2021 - 2023 Edwin Njeru (mailnjeru@gmail.com)
  *
  * This program is free software: you can redistribute it and/or modify
@@ -17,7 +17,6 @@ package io.github.erp.web.rest;
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-
 import static org.elasticsearch.index.query.QueryBuilders.*;
 
 import io.github.erp.repository.FiscalQuarterRepository;
