1c7bf9185afec87c1c9940fa5b20d59911c39b7a
Edwin Njeru <mailnjeru@gmail.com>
2023-11-10 09:33:07 +0300

    Implement  summary reports

--- scripts/maintain-entities.cmd
+++ scripts/maintain-entities.cmd
@@ -1,5 +1,5 @@
 @REM
-@REM Erp System - Mark VII No 1 (Gideon Series) Server ver 1.5.5
+@REM Erp System - Mark VII No 2 (Gideon Series) Server ver 1.5.6
 @REM Copyright © 2021 - 2023 Edwin Njeru (mailnjeru@gmail.com)
 @REM
 @REM This program is free software: you can redistribute it and/or modify
--- src/main/java/io/github/erp/internal/repository/InternalWIPProjectDealerSummaryReportRepository.java
+++ src/main/java/io/github/erp/internal/repository/InternalWIPProjectDealerSummaryReportRepository.java
@@ -1,7 +1,7 @@
 package io.github.erp.internal.repository;
 
 /*-
- * Erp System - Mark VII No 1 (Gideon Series) Server ver 1.5.5
+ * Erp System - Mark VII No 2 (Gideon Series) Server ver 1.5.6
  * Copyright © 2021 - 2023 Edwin Njeru (mailnjeru@gmail.com)
  *
  * This program is free software: you can redistribute it and/or modify
@@ -17,7 +17,6 @@ package io.github.erp.internal.repository;
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-
 import io.github.erp.domain.WorkInProgressRegistration;
 import io.github.erp.domain.WorkInProgressReport;
 import io.github.erp.domain.WorkInProgressReportREPO;
