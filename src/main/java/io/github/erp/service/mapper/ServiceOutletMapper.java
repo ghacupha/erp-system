2de1ab05e310ae2f1d058c9de6ce6f4dcb48ae83
Edwin Njeru <mailnjeru@gmail.com>
2023-11-14 19:59:07 +0300

    Adding workflows for WIP overview report

--- src/main/java/io/github/erp/service/mapper/ServiceOutletMapper.java
+++ src/main/java/io/github/erp/service/mapper/ServiceOutletMapper.java
@@ -1,7 +1,7 @@
 package io.github.erp.service.mapper;
 
 /*-
- * Erp System - Mark VII No 3 (Gideon Series) Server ver 1.5.7
+ * Erp System - Mark VII No 4 (Gideon Series) Server ver 1.5.8
  * Copyright © 2021 - 2023 Edwin Njeru (mailnjeru@gmail.com)
  *
  * This program is free software: you can redistribute it and/or modify
@@ -17,7 +17,6 @@ package io.github.erp.service.mapper;
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-
 import io.github.erp.domain.ServiceOutlet;
 import io.github.erp.service.dto.ServiceOutletDTO;
 import java.util.Set;
