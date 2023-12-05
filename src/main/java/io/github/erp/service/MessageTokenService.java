e7049fd7d9e4a00d80cfb318f617eb991d635c0b
Edwin Njeru <mailnjeru@gmail.com>
2023-12-05 16:10:46 +0300

    Final version in the hilkiah series release

--- src/main/java/io/github/erp/service/MessageTokenService.java
+++ src/main/java/io/github/erp/service/MessageTokenService.java
@@ -1,8 +1,8 @@
 package io.github.erp.service;
 
 /*-
- * Erp System - Mark VIII No 2 (Hilkiah Series) Server ver 1.6.1
- * Copyright © 2021 - 2023 Edwin Njeru (mailnjeru@gmail.com)
+ * Erp System - Mark VIII No 3 (Hilkiah Series) Server ver 1.6.2
+ * Copyright © 2021 - 2023 Edwin Njeru and the ERP System Contributors (mailnjeru@gmail.com)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -17,7 +17,6 @@ package io.github.erp.service;
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-
 import io.github.erp.service.dto.MessageTokenDTO;
 import java.util.Optional;
 import org.springframework.data.domain.Page;
