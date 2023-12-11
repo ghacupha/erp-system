59590936b0f9bdae9e14e7a62ffde74454e41cef
Edwin Njeru <mailnjeru@gmail.com>
2023-12-11 14:06:20 +0300

    Create autonomous-report entity

---
+++ src/main/java/io/github/erp/repository/search/AutonomousReportSearchRepository.java
@@ -0,0 +1,65 @@
+package io.github.erp.repository.search;
+
+/*-
+ * Erp System - Mark IX No 1 (Iddo Series) Server ver 1.6.3
+ * Copyright © 2021 - 2023 Edwin Njeru and the ERP System Contributors (mailnjeru@gmail.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import static org.elasticsearch.index.query.QueryBuilders.queryStringQuery;
+
+import io.github.erp.domain.AutonomousReport;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
+import org.springframework.data.elasticsearch.core.SearchHit;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQuery;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+/**
+ * Spring Data Elasticsearch repository for the {@link AutonomousReport} entity.
+ */
+public interface AutonomousReportSearchRepository
+    extends ElasticsearchRepository<AutonomousReport, Long>, AutonomousReportSearchRepositoryInternal {}
+
+interface AutonomousReportSearchRepositoryInternal {
+    Page<AutonomousReport> search(String query, Pageable pageable);
+}
+
+class AutonomousReportSearchRepositoryInternalImpl implements AutonomousReportSearchRepositoryInternal {
+
+    private final ElasticsearchRestTemplate elasticsearchTemplate;
+
+    AutonomousReportSearchRepositoryInternalImpl(ElasticsearchRestTemplate elasticsearchTemplate) {
+        this.elasticsearchTemplate = elasticsearchTemplate;
+    }
+
+    @Override
+    public Page<AutonomousReport> search(String query, Pageable pageable) {
+        NativeSearchQuery nativeSearchQuery = new NativeSearchQuery(queryStringQuery(query));
+        nativeSearchQuery.setPageable(pageable);
+        List<AutonomousReport> hits = elasticsearchTemplate
+            .search(nativeSearchQuery, AutonomousReport.class)
+            .map(SearchHit::getContent)
+            .stream()
+            .collect(Collectors.toList());
+
+        return new PageImpl<>(hits, pageable, hits.size());
+    }
+}
