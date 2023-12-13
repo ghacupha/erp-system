2aba5bb654f5ecab7f5c348b4a6be6279b07e9ae
Edwin Njeru <mailnjeru@gmail.com>
2023-12-13 10:27:06 +0300

    Abstract user-details service into interface

---
+++ src/main/java/io/github/erp/internal/service/InternalUserDetailServiceImpl.java
@@ -0,0 +1,61 @@
+package io.github.erp.internal.service;
+
+import io.github.erp.domain.ApplicationUser;
+import io.github.erp.internal.repository.InternalApplicationUserRepository;
+import io.github.erp.repository.UserRepository;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Transactional
+@Service
+public class InternalUserDetailServiceImpl implements InternalUserDetailService {
+
+    private final UserRepository userRepository;
+    private final InternalApplicationUserRepository internalApplicationUserRepository;
+
+    public InternalUserDetailServiceImpl(UserRepository userRepository, InternalApplicationUserRepository internalApplicationUserRepository) {
+        this.userRepository = userRepository;
+        this.internalApplicationUserRepository = internalApplicationUserRepository;
+    }
+
+    @Override
+    public Optional<ApplicationUser> getCurrentApplicationUser() {
+
+        return getCorrespondingApplicationUser(getCurrentUserDetails());
+    }
+
+    private Optional<ApplicationUser> getCorrespondingApplicationUser(UserDetails userDetails) {
+
+        var ref = new Object() {
+            Optional<ApplicationUser> reportUser = Optional.empty();
+        };
+
+        userRepository.findOneByLogin(userDetails.getUsername()).ifPresentOrElse(
+            user -> internalApplicationUserRepository.findApplicationUserBySystemIdentity(user)
+                .ifPresentOrElse(
+                    applicationUser -> ref.reportUser = Optional.of(applicationUser),
+                    () -> {throw new UsernameNotFoundException("We do not an application-user-id corresponding to user : " + user.getFirstName());}
+                ),
+            () -> {throw new UsernameNotFoundException("We could not retrieve user by username: " + userDetails.getUsername());}
+        );
+
+        return ref.reportUser;
+    }
+
+    private UserDetails getCurrentUserDetails() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && authentication.getPrincipal() instanceof UserDetails) {
+            return (UserDetails) authentication.getPrincipal();
+        }else {
+            // Handle case where user details are not found or not of the expected type
+            throw new UsernameNotFoundException("Unfortunately we cannot account for the current user");
+        }
+    }
+
+}
