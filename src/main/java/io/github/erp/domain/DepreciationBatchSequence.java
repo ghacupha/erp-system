2f90f77c7d889d1cbc1d45bc41456f2d1b3ca330
Edwin Njeru <mailnjeru@gmail.com>
2024-01-04 18:45:59 +0300

    Enable metamodel filtering on the depreciation-batch entity

--- src/main/java/io/github/erp/domain/DepreciationBatchSequence.java
+++ src/main/java/io/github/erp/domain/DepreciationBatchSequence.java
@@ -34,7 +34,7 @@ import org.hibernate.annotations.CacheConcurrencyStrategy;
 @Table(name = "depreciation_batch_sequence")
 @Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
 @org.springframework.data.elasticsearch.annotations.Document(indexName = "depreciationbatchsequence")
-public class DepreciationBatchSequence implements Serializable {
+public class DepreciationBatchSequence extends AbstractIdentifiableEntity implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
