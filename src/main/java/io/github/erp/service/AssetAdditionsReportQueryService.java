cf29f32faeb6f3872c846c843058e11e45d972fa
Edwin Njeru <mailnjeru@gmail.com>
2024-01-26 08:04:27 +0300

    Create asset-additions-report entity

---
+++ src/main/java/io/github/erp/service/AssetAdditionsReportQueryService.java
@@ -0,0 +1,142 @@
+package io.github.erp.service;
+
+/*-
+ * Erp System - Mark X No 2 (Jehoiada Series) Server ver 1.7.1
+ * Copyright © 2021 - 2024 Edwin Njeru and the ERP System Contributors (mailnjeru@gmail.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import io.github.erp.domain.*; // for static metamodels
+import io.github.erp.domain.AssetAdditionsReport;
+import io.github.erp.repository.AssetAdditionsReportRepository;
+import io.github.erp.repository.search.AssetAdditionsReportSearchRepository;
+import io.github.erp.service.criteria.AssetAdditionsReportCriteria;
+import io.github.erp.service.dto.AssetAdditionsReportDTO;
+import io.github.erp.service.mapper.AssetAdditionsReportMapper;
+import java.util.List;
+import javax.persistence.criteria.JoinType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import tech.jhipster.service.QueryService;
+
+/**
+ * Service for executing complex queries for {@link AssetAdditionsReport} entities in the database.
+ * The main input is a {@link AssetAdditionsReportCriteria} which gets converted to {@link Specification},
+ * in a way that all the filters must apply.
+ * It returns a {@link List} of {@link AssetAdditionsReportDTO} or a {@link Page} of {@link AssetAdditionsReportDTO} which fulfills the criteria.
+ */
+@Service
+@Transactional(readOnly = true)
+public class AssetAdditionsReportQueryService extends QueryService<AssetAdditionsReport> {
+
+    private final Logger log = LoggerFactory.getLogger(AssetAdditionsReportQueryService.class);
+
+    private final AssetAdditionsReportRepository assetAdditionsReportRepository;
+
+    private final AssetAdditionsReportMapper assetAdditionsReportMapper;
+
+    private final AssetAdditionsReportSearchRepository assetAdditionsReportSearchRepository;
+
+    public AssetAdditionsReportQueryService(
+        AssetAdditionsReportRepository assetAdditionsReportRepository,
+        AssetAdditionsReportMapper assetAdditionsReportMapper,
+        AssetAdditionsReportSearchRepository assetAdditionsReportSearchRepository
+    ) {
+        this.assetAdditionsReportRepository = assetAdditionsReportRepository;
+        this.assetAdditionsReportMapper = assetAdditionsReportMapper;
+        this.assetAdditionsReportSearchRepository = assetAdditionsReportSearchRepository;
+    }
+
+    /**
+     * Return a {@link List} of {@link AssetAdditionsReportDTO} which matches the criteria from the database.
+     * @param criteria The object which holds all the filters, which the entities should match.
+     * @return the matching entities.
+     */
+    @Transactional(readOnly = true)
+    public List<AssetAdditionsReportDTO> findByCriteria(AssetAdditionsReportCriteria criteria) {
+        log.debug("find by criteria : {}", criteria);
+        final Specification<AssetAdditionsReport> specification = createSpecification(criteria);
+        return assetAdditionsReportMapper.toDto(assetAdditionsReportRepository.findAll(specification));
+    }
+
+    /**
+     * Return a {@link Page} of {@link AssetAdditionsReportDTO} which matches the criteria from the database.
+     * @param criteria The object which holds all the filters, which the entities should match.
+     * @param page The page, which should be returned.
+     * @return the matching entities.
+     */
+    @Transactional(readOnly = true)
+    public Page<AssetAdditionsReportDTO> findByCriteria(AssetAdditionsReportCriteria criteria, Pageable page) {
+        log.debug("find by criteria : {}, page: {}", criteria, page);
+        final Specification<AssetAdditionsReport> specification = createSpecification(criteria);
+        return assetAdditionsReportRepository.findAll(specification, page).map(assetAdditionsReportMapper::toDto);
+    }
+
+    /**
+     * Return the number of matching entities in the database.
+     * @param criteria The object which holds all the filters, which the entities should match.
+     * @return the number of matching entities.
+     */
+    @Transactional(readOnly = true)
+    public long countByCriteria(AssetAdditionsReportCriteria criteria) {
+        log.debug("count by criteria : {}", criteria);
+        final Specification<AssetAdditionsReport> specification = createSpecification(criteria);
+        return assetAdditionsReportRepository.count(specification);
+    }
+
+    /**
+     * Function to convert {@link AssetAdditionsReportCriteria} to a {@link Specification}
+     * @param criteria The object which holds all the filters, which the entities should match.
+     * @return the matching {@link Specification} of the entity.
+     */
+    protected Specification<AssetAdditionsReport> createSpecification(AssetAdditionsReportCriteria criteria) {
+        Specification<AssetAdditionsReport> specification = Specification.where(null);
+        if (criteria != null) {
+            // This has to be called first, because the distinct method returns null
+            if (criteria.getDistinct() != null) {
+                specification = specification.and(distinct(criteria.getDistinct()));
+            }
+            if (criteria.getId() != null) {
+                specification = specification.and(buildRangeSpecification(criteria.getId(), AssetAdditionsReport_.id));
+            }
+            if (criteria.getTimeOfRequest() != null) {
+                specification =
+                    specification.and(buildRangeSpecification(criteria.getTimeOfRequest(), AssetAdditionsReport_.timeOfRequest));
+            }
+            if (criteria.getRequestId() != null) {
+                specification = specification.and(buildSpecification(criteria.getRequestId(), AssetAdditionsReport_.requestId));
+            }
+            if (criteria.getFileChecksum() != null) {
+                specification = specification.and(buildStringSpecification(criteria.getFileChecksum(), AssetAdditionsReport_.fileChecksum));
+            }
+            if (criteria.getTampered() != null) {
+                specification = specification.and(buildSpecification(criteria.getTampered(), AssetAdditionsReport_.tampered));
+            }
+            if (criteria.getFilename() != null) {
+                specification = specification.and(buildSpecification(criteria.getFilename(), AssetAdditionsReport_.filename));
+            }
+            if (criteria.getReportParameters() != null) {
+                specification =
+                    specification.and(buildStringSpecification(criteria.getReportParameters(), AssetAdditionsReport_.reportParameters));
+            }
+        }
+        return specification;
+    }
+}
---
+++ src/main/java/io/github/erp/web/rest/AssetAdditionsReportResource.java
@@ -0,0 +1,247 @@
+package io.github.erp.web.rest;
+
+/*-
+ * Erp System - Mark X No 2 (Jehoiada Series) Server ver 1.7.1
+ * Copyright © 2021 - 2024 Edwin Njeru and the ERP System Contributors (mailnjeru@gmail.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import static org.elasticsearch.index.query.QueryBuilders.*;
+
+import io.github.erp.repository.AssetAdditionsReportRepository;
+import io.github.erp.service.AssetAdditionsReportQueryService;
+import io.github.erp.service.AssetAdditionsReportService;
+import io.github.erp.service.criteria.AssetAdditionsReportCriteria;
+import io.github.erp.service.dto.AssetAdditionsReportDTO;
+import io.github.erp.web.rest.errors.BadRequestAlertException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.StreamSupport;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import tech.jhipster.web.util.HeaderUtil;
+import tech.jhipster.web.util.PaginationUtil;
+import tech.jhipster.web.util.ResponseUtil;
+
+/**
+ * REST controller for managing {@link io.github.erp.domain.AssetAdditionsReport}.
+ */
+@RestController
+@RequestMapping("/api")
+public class AssetAdditionsReportResource {
+
+    private final Logger log = LoggerFactory.getLogger(AssetAdditionsReportResource.class);
+
+    private static final String ENTITY_NAME = "assetAdditionsReport";
+
+    @Value("${jhipster.clientApp.name}")
+    private String applicationName;
+
+    private final AssetAdditionsReportService assetAdditionsReportService;
+
+    private final AssetAdditionsReportRepository assetAdditionsReportRepository;
+
+    private final AssetAdditionsReportQueryService assetAdditionsReportQueryService;
+
+    public AssetAdditionsReportResource(
+        AssetAdditionsReportService assetAdditionsReportService,
+        AssetAdditionsReportRepository assetAdditionsReportRepository,
+        AssetAdditionsReportQueryService assetAdditionsReportQueryService
+    ) {
+        this.assetAdditionsReportService = assetAdditionsReportService;
+        this.assetAdditionsReportRepository = assetAdditionsReportRepository;
+        this.assetAdditionsReportQueryService = assetAdditionsReportQueryService;
+    }
+
+    /**
+     * {@code POST  /asset-additions-reports} : Create a new assetAdditionsReport.
+     *
+     * @param assetAdditionsReportDTO the assetAdditionsReportDTO to create.
+     * @return the {@link ResponseEntity} with status {@code 201 (Created)} and with body the new assetAdditionsReportDTO, or with status {@code 400 (Bad Request)} if the assetAdditionsReport has already an ID.
+     * @throws URISyntaxException if the Location URI syntax is incorrect.
+     */
+    @PostMapping("/asset-additions-reports")
+    public ResponseEntity<AssetAdditionsReportDTO> createAssetAdditionsReport(
+        @Valid @RequestBody AssetAdditionsReportDTO assetAdditionsReportDTO
+    ) throws URISyntaxException {
+        log.debug("REST request to save AssetAdditionsReport : {}", assetAdditionsReportDTO);
+        if (assetAdditionsReportDTO.getId() != null) {
+            throw new BadRequestAlertException("A new assetAdditionsReport cannot already have an ID", ENTITY_NAME, "idexists");
+        }
+        AssetAdditionsReportDTO result = assetAdditionsReportService.save(assetAdditionsReportDTO);
+        return ResponseEntity
+            .created(new URI("/api/asset-additions-reports/" + result.getId()))
+            .headers(HeaderUtil.createEntityCreationAlert(applicationName, true, ENTITY_NAME, result.getId().toString()))
+            .body(result);
+    }
+
+    /**
+     * {@code PUT  /asset-additions-reports/:id} : Updates an existing assetAdditionsReport.
+     *
+     * @param id the id of the assetAdditionsReportDTO to save.
+     * @param assetAdditionsReportDTO the assetAdditionsReportDTO to update.
+     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body the updated assetAdditionsReportDTO,
+     * or with status {@code 400 (Bad Request)} if the assetAdditionsReportDTO is not valid,
+     * or with status {@code 500 (Internal Server Error)} if the assetAdditionsReportDTO couldn't be updated.
+     * @throws URISyntaxException if the Location URI syntax is incorrect.
+     */
+    @PutMapping("/asset-additions-reports/{id}")
+    public ResponseEntity<AssetAdditionsReportDTO> updateAssetAdditionsReport(
+        @PathVariable(value = "id", required = false) final Long id,
+        @Valid @RequestBody AssetAdditionsReportDTO assetAdditionsReportDTO
+    ) throws URISyntaxException {
+        log.debug("REST request to update AssetAdditionsReport : {}, {}", id, assetAdditionsReportDTO);
+        if (assetAdditionsReportDTO.getId() == null) {
+            throw new BadRequestAlertException("Invalid id", ENTITY_NAME, "idnull");
+        }
+        if (!Objects.equals(id, assetAdditionsReportDTO.getId())) {
+            throw new BadRequestAlertException("Invalid ID", ENTITY_NAME, "idinvalid");
+        }
+
+        if (!assetAdditionsReportRepository.existsById(id)) {
+            throw new BadRequestAlertException("Entity not found", ENTITY_NAME, "idnotfound");
+        }
+
+        AssetAdditionsReportDTO result = assetAdditionsReportService.save(assetAdditionsReportDTO);
+        return ResponseEntity
+            .ok()
+            .headers(HeaderUtil.createEntityUpdateAlert(applicationName, true, ENTITY_NAME, assetAdditionsReportDTO.getId().toString()))
+            .body(result);
+    }
+
+    /**
+     * {@code PATCH  /asset-additions-reports/:id} : Partial updates given fields of an existing assetAdditionsReport, field will ignore if it is null
+     *
+     * @param id the id of the assetAdditionsReportDTO to save.
+     * @param assetAdditionsReportDTO the assetAdditionsReportDTO to update.
+     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body the updated assetAdditionsReportDTO,
+     * or with status {@code 400 (Bad Request)} if the assetAdditionsReportDTO is not valid,
+     * or with status {@code 404 (Not Found)} if the assetAdditionsReportDTO is not found,
+     * or with status {@code 500 (Internal Server Error)} if the assetAdditionsReportDTO couldn't be updated.
+     * @throws URISyntaxException if the Location URI syntax is incorrect.
+     */
+    @PatchMapping(value = "/asset-additions-reports/{id}", consumes = { "application/json", "application/merge-patch+json" })
+    public ResponseEntity<AssetAdditionsReportDTO> partialUpdateAssetAdditionsReport(
+        @PathVariable(value = "id", required = false) final Long id,
+        @NotNull @RequestBody AssetAdditionsReportDTO assetAdditionsReportDTO
+    ) throws URISyntaxException {
+        log.debug("REST request to partial update AssetAdditionsReport partially : {}, {}", id, assetAdditionsReportDTO);
+        if (assetAdditionsReportDTO.getId() == null) {
+            throw new BadRequestAlertException("Invalid id", ENTITY_NAME, "idnull");
+        }
+        if (!Objects.equals(id, assetAdditionsReportDTO.getId())) {
+            throw new BadRequestAlertException("Invalid ID", ENTITY_NAME, "idinvalid");
+        }
+
+        if (!assetAdditionsReportRepository.existsById(id)) {
+            throw new BadRequestAlertException("Entity not found", ENTITY_NAME, "idnotfound");
+        }
+
+        Optional<AssetAdditionsReportDTO> result = assetAdditionsReportService.partialUpdate(assetAdditionsReportDTO);
+
+        return ResponseUtil.wrapOrNotFound(
+            result,
+            HeaderUtil.createEntityUpdateAlert(applicationName, true, ENTITY_NAME, assetAdditionsReportDTO.getId().toString())
+        );
+    }
+
+    /**
+     * {@code GET  /asset-additions-reports} : get all the assetAdditionsReports.
+     *
+     * @param pageable the pagination information.
+     * @param criteria the criteria which the requested entities should match.
+     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and the list of assetAdditionsReports in body.
+     */
+    @GetMapping("/asset-additions-reports")
+    public ResponseEntity<List<AssetAdditionsReportDTO>> getAllAssetAdditionsReports(
+        AssetAdditionsReportCriteria criteria,
+        Pageable pageable
+    ) {
+        log.debug("REST request to get AssetAdditionsReports by criteria: {}", criteria);
+        Page<AssetAdditionsReportDTO> page = assetAdditionsReportQueryService.findByCriteria(criteria, pageable);
+        HttpHeaders headers = PaginationUtil.generatePaginationHttpHeaders(ServletUriComponentsBuilder.fromCurrentRequest(), page);
+        return ResponseEntity.ok().headers(headers).body(page.getContent());
+    }
+
+    /**
+     * {@code GET  /asset-additions-reports/count} : count all the assetAdditionsReports.
+     *
+     * @param criteria the criteria which the requested entities should match.
+     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and the count in body.
+     */
+    @GetMapping("/asset-additions-reports/count")
+    public ResponseEntity<Long> countAssetAdditionsReports(AssetAdditionsReportCriteria criteria) {
+        log.debug("REST request to count AssetAdditionsReports by criteria: {}", criteria);
+        return ResponseEntity.ok().body(assetAdditionsReportQueryService.countByCriteria(criteria));
+    }
+
+    /**
+     * {@code GET  /asset-additions-reports/:id} : get the "id" assetAdditionsReport.
+     *
+     * @param id the id of the assetAdditionsReportDTO to retrieve.
+     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body the assetAdditionsReportDTO, or with status {@code 404 (Not Found)}.
+     */
+    @GetMapping("/asset-additions-reports/{id}")
+    public ResponseEntity<AssetAdditionsReportDTO> getAssetAdditionsReport(@PathVariable Long id) {
+        log.debug("REST request to get AssetAdditionsReport : {}", id);
+        Optional<AssetAdditionsReportDTO> assetAdditionsReportDTO = assetAdditionsReportService.findOne(id);
+        return ResponseUtil.wrapOrNotFound(assetAdditionsReportDTO);
+    }
+
+    /**
+     * {@code DELETE  /asset-additions-reports/:id} : delete the "id" assetAdditionsReport.
+     *
+     * @param id the id of the assetAdditionsReportDTO to delete.
+     * @return the {@link ResponseEntity} with status {@code 204 (NO_CONTENT)}.
+     */
+    @DeleteMapping("/asset-additions-reports/{id}")
+    public ResponseEntity<Void> deleteAssetAdditionsReport(@PathVariable Long id) {
+        log.debug("REST request to delete AssetAdditionsReport : {}", id);
+        assetAdditionsReportService.delete(id);
+        return ResponseEntity
+            .noContent()
+            .headers(HeaderUtil.createEntityDeletionAlert(applicationName, true, ENTITY_NAME, id.toString()))
+            .build();
+    }
+
+    /**
+     * {@code SEARCH  /_search/asset-additions-reports?query=:query} : search for the assetAdditionsReport corresponding
+     * to the query.
+     *
+     * @param query the query of the assetAdditionsReport search.
+     * @param pageable the pagination information.
+     * @return the result of the search.
+     */
+    @GetMapping("/_search/asset-additions-reports")
+    public ResponseEntity<List<AssetAdditionsReportDTO>> searchAssetAdditionsReports(@RequestParam String query, Pageable pageable) {
+        log.debug("REST request to search for a page of AssetAdditionsReports for query {}", query);
+        Page<AssetAdditionsReportDTO> page = assetAdditionsReportService.search(query, pageable);
+        HttpHeaders headers = PaginationUtil.generatePaginationHttpHeaders(ServletUriComponentsBuilder.fromCurrentRequest(), page);
+        return ResponseEntity.ok().headers(headers).body(page.getContent());
+    }
+}
