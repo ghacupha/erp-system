f3c127d37d4e9bb532494ae3bc08495228b108af
Edwin Njeru <mailnjeru@gmail.com>
2024-02-16 15:15:16 +0300

    Release 1.7.3

--- src/main/java/io/github/erp/internal/service/InternalDepreciationEntryService.java
+++ src/main/java/io/github/erp/internal/service/InternalDepreciationEntryService.java
@@ -1,5 +1,23 @@
 package io.github.erp.internal.service;
 
+/*-
+ * Erp System - Mark X No 3 (Jehoiada Series) Server ver 1.7.3
+ * Copyright © 2021 - 2024 Edwin Njeru and the ERP System Contributors (mailnjeru@gmail.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import io.github.erp.domain.DepreciationEntry;
 import io.github.erp.service.dto.DepreciationEntryDTO;
 import org.springframework.data.domain.Page;
