d6ca44f46ec39338fedd2e0b1fcea528a6626369
Edwin Njeru <mailnjeru@gmail.com>
2024-02-20 07:08:08 +0300

    Implement nbv-report

---
+++ src/main/java/io/github/erp/service/criteria/NbvReportCriteria.java
@@ -0,0 +1,327 @@
+package io.github.erp.service.criteria;
+
+/*-
+ * Erp System - Mark X No 3 (Jehoiada Series) Server ver 1.7.3
+ * Copyright © 2021 - 2024 Edwin Njeru and the ERP System Contributors (mailnjeru@gmail.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import java.io.Serializable;
+import java.util.Objects;
+import tech.jhipster.service.Criteria;
+import tech.jhipster.service.filter.BooleanFilter;
+import tech.jhipster.service.filter.DoubleFilter;
+import tech.jhipster.service.filter.Filter;
+import tech.jhipster.service.filter.FloatFilter;
+import tech.jhipster.service.filter.IntegerFilter;
+import tech.jhipster.service.filter.LongFilter;
+import tech.jhipster.service.filter.StringFilter;
+import tech.jhipster.service.filter.UUIDFilter;
+import tech.jhipster.service.filter.ZonedDateTimeFilter;
+
+/**
+ * Criteria class for the {@link io.github.erp.domain.NbvReport} entity. This class is used
+ * in {@link io.github.erp.web.rest.NbvReportResource} to receive all the possible filtering options from
+ * the Http GET request parameters.
+ * For example the following could be a valid request:
+ * {@code /nbv-reports?id.greaterThan=5&attr1.contains=something&attr2.specified=false}
+ * As Spring is unable to properly convert the types, unless specific {@link Filter} class are used, we need to use
+ * fix type specific filters.
+ */
+public class NbvReportCriteria implements Serializable, Criteria {
+
+    private static final long serialVersionUID = 1L;
+
+    private LongFilter id;
+
+    private StringFilter reportName;
+
+    private ZonedDateTimeFilter timeOfReportRequest;
+
+    private StringFilter fileChecksum;
+
+    private BooleanFilter tampered;
+
+    private UUIDFilter filename;
+
+    private StringFilter reportParameters;
+
+    private LongFilter requestedById;
+
+    private LongFilter depreciationPeriodId;
+
+    private LongFilter serviceOutletId;
+
+    private LongFilter assetCategoryId;
+
+    private Boolean distinct;
+
+    public NbvReportCriteria() {}
+
+    public NbvReportCriteria(NbvReportCriteria other) {
+        this.id = other.id == null ? null : other.id.copy();
+        this.reportName = other.reportName == null ? null : other.reportName.copy();
+        this.timeOfReportRequest = other.timeOfReportRequest == null ? null : other.timeOfReportRequest.copy();
+        this.fileChecksum = other.fileChecksum == null ? null : other.fileChecksum.copy();
+        this.tampered = other.tampered == null ? null : other.tampered.copy();
+        this.filename = other.filename == null ? null : other.filename.copy();
+        this.reportParameters = other.reportParameters == null ? null : other.reportParameters.copy();
+        this.requestedById = other.requestedById == null ? null : other.requestedById.copy();
+        this.depreciationPeriodId = other.depreciationPeriodId == null ? null : other.depreciationPeriodId.copy();
+        this.serviceOutletId = other.serviceOutletId == null ? null : other.serviceOutletId.copy();
+        this.assetCategoryId = other.assetCategoryId == null ? null : other.assetCategoryId.copy();
+        this.distinct = other.distinct;
+    }
+
+    @Override
+    public NbvReportCriteria copy() {
+        return new NbvReportCriteria(this);
+    }
+
+    public LongFilter getId() {
+        return id;
+    }
+
+    public LongFilter id() {
+        if (id == null) {
+            id = new LongFilter();
+        }
+        return id;
+    }
+
+    public void setId(LongFilter id) {
+        this.id = id;
+    }
+
+    public StringFilter getReportName() {
+        return reportName;
+    }
+
+    public StringFilter reportName() {
+        if (reportName == null) {
+            reportName = new StringFilter();
+        }
+        return reportName;
+    }
+
+    public void setReportName(StringFilter reportName) {
+        this.reportName = reportName;
+    }
+
+    public ZonedDateTimeFilter getTimeOfReportRequest() {
+        return timeOfReportRequest;
+    }
+
+    public ZonedDateTimeFilter timeOfReportRequest() {
+        if (timeOfReportRequest == null) {
+            timeOfReportRequest = new ZonedDateTimeFilter();
+        }
+        return timeOfReportRequest;
+    }
+
+    public void setTimeOfReportRequest(ZonedDateTimeFilter timeOfReportRequest) {
+        this.timeOfReportRequest = timeOfReportRequest;
+    }
+
+    public StringFilter getFileChecksum() {
+        return fileChecksum;
+    }
+
+    public StringFilter fileChecksum() {
+        if (fileChecksum == null) {
+            fileChecksum = new StringFilter();
+        }
+        return fileChecksum;
+    }
+
+    public void setFileChecksum(StringFilter fileChecksum) {
+        this.fileChecksum = fileChecksum;
+    }
+
+    public BooleanFilter getTampered() {
+        return tampered;
+    }
+
+    public BooleanFilter tampered() {
+        if (tampered == null) {
+            tampered = new BooleanFilter();
+        }
+        return tampered;
+    }
+
+    public void setTampered(BooleanFilter tampered) {
+        this.tampered = tampered;
+    }
+
+    public UUIDFilter getFilename() {
+        return filename;
+    }
+
+    public UUIDFilter filename() {
+        if (filename == null) {
+            filename = new UUIDFilter();
+        }
+        return filename;
+    }
+
+    public void setFilename(UUIDFilter filename) {
+        this.filename = filename;
+    }
+
+    public StringFilter getReportParameters() {
+        return reportParameters;
+    }
+
+    public StringFilter reportParameters() {
+        if (reportParameters == null) {
+            reportParameters = new StringFilter();
+        }
+        return reportParameters;
+    }
+
+    public void setReportParameters(StringFilter reportParameters) {
+        this.reportParameters = reportParameters;
+    }
+
+    public LongFilter getRequestedById() {
+        return requestedById;
+    }
+
+    public LongFilter requestedById() {
+        if (requestedById == null) {
+            requestedById = new LongFilter();
+        }
+        return requestedById;
+    }
+
+    public void setRequestedById(LongFilter requestedById) {
+        this.requestedById = requestedById;
+    }
+
+    public LongFilter getDepreciationPeriodId() {
+        return depreciationPeriodId;
+    }
+
+    public LongFilter depreciationPeriodId() {
+        if (depreciationPeriodId == null) {
+            depreciationPeriodId = new LongFilter();
+        }
+        return depreciationPeriodId;
+    }
+
+    public void setDepreciationPeriodId(LongFilter depreciationPeriodId) {
+        this.depreciationPeriodId = depreciationPeriodId;
+    }
+
+    public LongFilter getServiceOutletId() {
+        return serviceOutletId;
+    }
+
+    public LongFilter serviceOutletId() {
+        if (serviceOutletId == null) {
+            serviceOutletId = new LongFilter();
+        }
+        return serviceOutletId;
+    }
+
+    public void setServiceOutletId(LongFilter serviceOutletId) {
+        this.serviceOutletId = serviceOutletId;
+    }
+
+    public LongFilter getAssetCategoryId() {
+        return assetCategoryId;
+    }
+
+    public LongFilter assetCategoryId() {
+        if (assetCategoryId == null) {
+            assetCategoryId = new LongFilter();
+        }
+        return assetCategoryId;
+    }
+
+    public void setAssetCategoryId(LongFilter assetCategoryId) {
+        this.assetCategoryId = assetCategoryId;
+    }
+
+    public Boolean getDistinct() {
+        return distinct;
+    }
+
+    public void setDistinct(Boolean distinct) {
+        this.distinct = distinct;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        final NbvReportCriteria that = (NbvReportCriteria) o;
+        return (
+            Objects.equals(id, that.id) &&
+            Objects.equals(reportName, that.reportName) &&
+            Objects.equals(timeOfReportRequest, that.timeOfReportRequest) &&
+            Objects.equals(fileChecksum, that.fileChecksum) &&
+            Objects.equals(tampered, that.tampered) &&
+            Objects.equals(filename, that.filename) &&
+            Objects.equals(reportParameters, that.reportParameters) &&
+            Objects.equals(requestedById, that.requestedById) &&
+            Objects.equals(depreciationPeriodId, that.depreciationPeriodId) &&
+            Objects.equals(serviceOutletId, that.serviceOutletId) &&
+            Objects.equals(assetCategoryId, that.assetCategoryId) &&
+            Objects.equals(distinct, that.distinct)
+        );
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(
+            id,
+            reportName,
+            timeOfReportRequest,
+            fileChecksum,
+            tampered,
+            filename,
+            reportParameters,
+            requestedById,
+            depreciationPeriodId,
+            serviceOutletId,
+            assetCategoryId,
+            distinct
+        );
+    }
+
+    // prettier-ignore
+    @Override
+    public String toString() {
+        return "NbvReportCriteria{" +
+            (id != null ? "id=" + id + ", " : "") +
+            (reportName != null ? "reportName=" + reportName + ", " : "") +
+            (timeOfReportRequest != null ? "timeOfReportRequest=" + timeOfReportRequest + ", " : "") +
+            (fileChecksum != null ? "fileChecksum=" + fileChecksum + ", " : "") +
+            (tampered != null ? "tampered=" + tampered + ", " : "") +
+            (filename != null ? "filename=" + filename + ", " : "") +
+            (reportParameters != null ? "reportParameters=" + reportParameters + ", " : "") +
+            (requestedById != null ? "requestedById=" + requestedById + ", " : "") +
+            (depreciationPeriodId != null ? "depreciationPeriodId=" + depreciationPeriodId + ", " : "") +
+            (serviceOutletId != null ? "serviceOutletId=" + serviceOutletId + ", " : "") +
+            (assetCategoryId != null ? "assetCategoryId=" + assetCategoryId + ", " : "") +
+            (distinct != null ? "distinct=" + distinct + ", " : "") +
+            "}";
+    }
+}
