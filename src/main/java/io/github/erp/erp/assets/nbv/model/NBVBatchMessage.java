0c8b33622cb73fd7bb3dee30ad68b78f0af8e4b6
Edwin Njeru <mailnjeru@gmail.com>
2024-02-21 14:16:51 +0300

    Models for NBV calculation

---
+++ src/main/java/io/github/erp/erp/assets/nbv/model/NBVBatchMessage.java
@@ -0,0 +1,38 @@
+package io.github.erp.erp.assets.nbv.model;
+
+import io.github.erp.erp.assets.depreciation.context.DepreciationContextInstance;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class NBVBatchMessage implements Serializable {
+
+    private UUID messageCorrelationId;
+    private long jobId;
+    private long batchId;
+    private int batchSize;
+    private List<Long> assetIds;
+    private LocalDateTime createdAt;
+    private int startIndex;
+    private int endIndex;
+    private boolean isLastBatch;
+    private int enqueuedCount;
+    private int sequenceNumber;
+    private int totalItems;
+    private DepreciationContextInstance depreciationContextInstance;
+
+    private boolean processed;
+    private int processedMessagesCount;
+    private int numberOfBatches;
+}
