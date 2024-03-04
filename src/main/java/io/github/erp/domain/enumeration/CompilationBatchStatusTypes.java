919957e6d57434c976174ad5b1bd5be8e2eb1273
Edwin Njeru <mailnjeru@gmail.com>
2024-03-04 17:39:51 +0300

    Create monthly-prepayment-outstanding-report-item entity

--- src/main/java/io/github/erp/domain/enumeration/CompilationBatchStatusTypes.java
+++ src/main/java/io/github/erp/domain/enumeration/CompilationBatchStatusTypes.java
@@ -17,6 +17,7 @@ package io.github.erp.domain.enumeration;
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
+
 /**
  * The CompilationBatchStatusTypes enumeration.
  */
--- src/test/java/io/github/erp/repository/search/ReportStatusSearchRepositoryMockConfiguration.java
+++ src/test/java/io/github/erp/repository/search/ReportStatusSearchRepositoryMockConfiguration.java
@@ -17,6 +17,7 @@ package io.github.erp.repository.search;
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
+
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Configuration;
 
