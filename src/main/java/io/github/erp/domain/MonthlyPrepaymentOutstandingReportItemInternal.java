6e59e8f591fad1f9b811b6968a5575a1aa71e1eb
Edwin Njeru <mailnjeru@gmail.com>
2024-03-05 12:21:57 +0300

    Create mapper for monthly prepayment report

--- src/main/java/io/github/erp/domain/MonthlyPrepaymentOutstandingReportItemInternal.java
+++ src/main/java/io/github/erp/domain/MonthlyPrepaymentOutstandingReportItemInternal.java
@@ -5,7 +5,7 @@ import java.time.LocalDate;
 
 public interface MonthlyPrepaymentOutstandingReportItemInternal {
 
-    // Long getId();
+    Long getId();
 
     LocalDate getFiscalMonthEndDate();
 
--- src/main/java/io/github/erp/internal/repository/InternalMonthlyPrepaymentOutstandingReportItemRepository.java
+++ src/main/java/io/github/erp/internal/repository/InternalMonthlyPrepaymentOutstandingReportItemRepository.java
@@ -44,48 +44,47 @@ public interface InternalMonthlyPrepaymentOutstandingReportItemRepository
         value = "" +
             "WITH RECURSIVE end_month_series AS (" +
             "    SELECT" +
-            "        DATE_TRUNC('month', :fiscalStartDate) AS start_date," +
-            "        (DATE_TRUNC('month', :fiscalStartDate) + INTERVAL '1 month' - INTERVAL '1 day') AS end_date" +
-            "    UNION ALL" +
+            "        DATE_TRUNC('month', CAST(:fiscalStartDate AS DATE)) AS start_date, " +
+            "        (DATE_TRUNC('month', CAST(:fiscalStartDate AS DATE)) + INTERVAL '1 month' - INTERVAL '1 day') AS end_date" +
+            "    UNION ALL " +
             "    SELECT" +
-            "        (DATE_TRUNC('month', ems.start_date) + INTERVAL '1 month') AS start_date," +
+            "        (DATE_TRUNC('month', ems.start_date) + INTERVAL '1 month') AS start_date, " +
             "        ((DATE_TRUNC('month', ems.start_date) + INTERVAL '2 months' - INTERVAL '1 day')) AS end_date" + // TODO Review use of date casting
-            "    FROM" +
-            "        end_month_series ems" +
-            "    WHERE" +
-            "            ems.start_date < :fiscalEndDate " + // TODO set first day of the last month here
-            ")" +
-            " " +
-            "SELECT" +
-            "    ems.end_date AS FiscalMonthEndDate," +
-            "    total_prepayment.TotalPrepaymentAmount," +
-            "    COALESCE(SUM(ps.amortised_amount), 0) AS TotalAmortisedAmount," +
-            "    (total_prepayment.TotalPrepaymentAmount - COALESCE(SUM(ps.amortised_amount), 0)) AS TotalOutstandingAmount," +
-            "    COUNT(DISTINCT ps.id) AS NumberOfPrepaymentAccounts" +
-            "FROM" +
+            "    FROM " +
+            "        end_month_series ems " +
+            "    WHERE " +
+            "            ems.start_date < CAST(:fiscalEndDate AS DATE) " + // TODO set first day of the last month here
+            ") " +
+            " SELECT " +
+            "    ems.end_date AS FiscalMonthEndDate, " +
+            "    total_prepayment.TotalPrepaymentAmount, " +
+            "    COALESCE(SUM(ps.amortised_amount), 0) AS TotalAmortisedAmount, " +
+            "    (total_prepayment.TotalPrepaymentAmount - COALESCE(SUM(ps.amortised_amount), 0)) AS TotalOutstandingAmount, " +
+            "    COUNT(DISTINCT ps.id) AS NumberOfPrepaymentAccounts " +
+            " FROM " +
             "    end_month_series ems " +
             "        LEFT JOIN (" +
             "        SELECT" +
-            "            p.id," +
-            "            COALESCE(p.prepayment_amount, 0) AS prepayment_amount," +
-            "            COALESCE(pa.prepayment_amount, 0) AS amortised_amount," +
-            "            COALESCE(p.prepayment_amount, 0) - COALESCE(pa.prepayment_amount, 0) AS outstanding_amount," +
-            "            pa.fiscal_month_id," +
+            "            p.id, " +
+            "            COALESCE(p.prepayment_amount, 0) AS prepayment_amount, " +
+            "            COALESCE(pa.prepayment_amount, 0) AS amortised_amount, " +
+            "            COALESCE(p.prepayment_amount, 0) - COALESCE(pa.prepayment_amount, 0) AS outstanding_amount, " +
+            "            pa.fiscal_month_id, " +
             "            s.payment_date" +
             "        FROM" +
             "            prepayment_account p" +
             "                LEFT JOIN" +
             "            settlement s ON s.id = p.prepayment_transaction_id" +
             "                LEFT JOIN" +
-            "            prepayment_amortization pa ON p.id = pa.prepayment_account_id" +
-            "    ) ps ON TRUE" +
+            "            prepayment_amortization pa ON p.id = pa.prepayment_account_id " +
+            "    ) ps ON TRUE " +
             "        CROSS JOIN LATERAL (" +
             "        SELECT SUM(COALESCE(p.prepayment_amount, 0)) AS TotalPrepaymentAmount" +
             "        FROM prepayment_account p" +
             "                 LEFT JOIN settlement s ON s.id = p.prepayment_transaction_id" +
             "        WHERE s.payment_date <= ems.end_date" +
             "        ) AS total_prepayment" +
-            "WHERE" +
+            " WHERE " +
             "        ps.fiscal_month_id IN (" +
             "        SELECT fm.id" +
             "        FROM fiscal_month fm" +
@@ -95,9 +94,9 @@ public interface InternalMonthlyPrepaymentOutstandingReportItemRepository
             "        (ps.payment_date <= ems.end_date AND ps.payment_date IS NOT NULL)" +
             "        OR (ps.payment_date IS NULL AND ps.fiscal_month_id IS NOT NULL)" +
             "    )" +
-            "GROUP BY" +
-            "    ems.end_date, total_prepayment.TotalPrepaymentAmount" +
-            "ORDER BY" +
+            " GROUP BY" +
+            "    ems.end_date, total_prepayment.TotalPrepaymentAmount " +
+            " ORDER BY" +
             "    ems.end_date"
     )
     Page<MonthlyPrepaymentOutstandingReportItemInternal> findReportItemsByFiscalPeriod(
