83a6ffaeff8db6f8c54193a43766634d33995563
Edwin Njeru <mailnjeru@gmail.com>
2024-04-26 10:53:27 +0300

    Implement next number algorithm for prepayments and assets

--- src/main/java/io/github/erp/erp/index/DepreciationPeriodIndexingService.java
+++ src/main/java/io/github/erp/erp/index/DepreciationPeriodIndexingService.java
@@ -1,5 +1,21 @@
 package io.github.erp.erp.index;
 
+/*-
+ * Copyright © 2021 - 2024 Edwin Njeru and the ERP System Contributors (mailnjeru@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import com.google.common.collect.ImmutableList;
 import io.github.erp.erp.index.engine_v1.AbstractStartupRegisteredIndexService;
 import io.github.erp.erp.index.engine_v1.IndexingServiceChainSingleton;
