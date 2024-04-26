77174862596ddedf1c331e72bb243bca0c5c30eb
Edwin Njeru <mailnjeru@gmail.com>
2024-04-26 11:00:03 +0300

    Release version 1.7.9

--- src/main/java/io/github/erp/internal/report/autonomousReport/PrepaymentAccountExportService.java
+++ src/main/java/io/github/erp/internal/report/autonomousReport/PrepaymentAccountExportService.java
@@ -1,5 +1,23 @@
 
 /*-
+ * Erp System - Mark X No 7 (Jehoiada Series) Server ver 1.7.9
+ * Copyright © 2021 - 2024 Edwin Njeru and the ERP System Contributors (mailnjeru@gmail.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*-
  * Copyright © 2021 - 2024 Edwin Njeru and the ERP System Contributors (mailnjeru@gmail.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
