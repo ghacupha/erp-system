f45493a90d9b84bbd6d7c54064a1b17a0ad40f53
Edwin Njeru <mailnjeru@gmail.com>
2024-05-08 12:38:16 +0300

    Implement indexing for users

--- src/main/java/io/github/erp/internal/service/applicationUser/InternalSystemUserService.java
+++ src/main/java/io/github/erp/internal/service/applicationUser/InternalSystemUserService.java
@@ -1,11 +1,13 @@
 package io.github.erp.internal.service.applicationUser;
 
+import io.github.erp.domain.User;
 import io.github.erp.service.dto.ApplicationUserDTO;
 import io.github.erp.service.dto.UserDTO;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * This is a service to bring common implementations of entities to the system generated
@@ -21,4 +23,10 @@ public interface InternalSystemUserService {
      * @return the list of entities.
      */
     List<UserDTO> search(String query);
+
+    /**
+     *
+     * @return List of users from the system generated User entity
+     */
+    Optional<List<User>> findAll();
 }
--- src/main/java/io/github/erp/internal/service/applicationUser/InternalSystemUserServiceImpl.java
+++ src/main/java/io/github/erp/internal/service/applicationUser/InternalSystemUserServiceImpl.java
@@ -1,19 +1,27 @@
 package io.github.erp.internal.service.applicationUser;
 
+import io.github.erp.domain.User;
+import io.github.erp.repository.UserRepository;
 import io.github.erp.repository.search.UserSearchRepository;
 import io.github.erp.service.dto.UserDTO;
+import io.github.erp.service.mapper.UserMapper;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
 @Service
 public class InternalSystemUserServiceImpl implements InternalSystemUserService {
 
+    private final UserMapper userMapper;
+    private final UserRepository userRepository;
     private final UserSearchRepository userSearchRepository;
 
-    public InternalSystemUserServiceImpl(UserSearchRepository userSearchRepository) {
+    public InternalSystemUserServiceImpl(UserMapper userMapper, UserRepository userRepository, UserSearchRepository userSearchRepository) {
+        this.userMapper = userMapper;
+        this.userRepository = userRepository;
         this.userSearchRepository = userSearchRepository;
     }
 
@@ -26,5 +34,11 @@ public class InternalSystemUserServiceImpl implements InternalSystemUserService 
     @Override
     public List<UserDTO> search(String query) {
         return StreamSupport.stream(userSearchRepository.search(query).spliterator(), false).map(UserDTO::new).collect(Collectors.toList());
+    }
+
+    public Optional<List<User>> findAll() {
+
+        return Optional.of(userRepository.findAll());
+
     }
 }
