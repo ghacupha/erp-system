06adb3ded0c08cfb90b79a9b3cf86f19e384ccaf
Edwin Njeru <mailnjeru@gmail.com>
2024-08-28 14:16:57 +0300

    Implement leaseLiability report items

---
+++ src/main/java/io/github/erp/web/rest/LeaseLiabilityPostingReportItemResource.java
@@ -0,0 +1,137 @@
+package io.github.erp.web.rest;
+
+/*-
+ * Erp System - Mark X No 9 (Jehoiada Series) Server ver 1.8.1
+ * Copyright © 2021 - 2024 Edwin Njeru and the ERP System Contributors (mailnjeru@gmail.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import static org.elasticsearch.index.query.QueryBuilders.*;
+
+import io.github.erp.repository.LeaseLiabilityPostingReportItemRepository;
+import io.github.erp.service.LeaseLiabilityPostingReportItemQueryService;
+import io.github.erp.service.LeaseLiabilityPostingReportItemService;
+import io.github.erp.service.criteria.LeaseLiabilityPostingReportItemCriteria;
+import io.github.erp.service.dto.LeaseLiabilityPostingReportItemDTO;
+import io.github.erp.web.rest.errors.BadRequestAlertException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.StreamSupport;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import tech.jhipster.web.util.HeaderUtil;
+import tech.jhipster.web.util.PaginationUtil;
+import tech.jhipster.web.util.ResponseUtil;
+
+/**
+ * REST controller for managing {@link io.github.erp.domain.LeaseLiabilityPostingReportItem}.
+ */
+@RestController
+@RequestMapping("/api")
+public class LeaseLiabilityPostingReportItemResource {
+
+    private final Logger log = LoggerFactory.getLogger(LeaseLiabilityPostingReportItemResource.class);
+
+    private final LeaseLiabilityPostingReportItemService leaseLiabilityPostingReportItemService;
+
+    private final LeaseLiabilityPostingReportItemRepository leaseLiabilityPostingReportItemRepository;
+
+    private final LeaseLiabilityPostingReportItemQueryService leaseLiabilityPostingReportItemQueryService;
+
+    public LeaseLiabilityPostingReportItemResource(
+        LeaseLiabilityPostingReportItemService leaseLiabilityPostingReportItemService,
+        LeaseLiabilityPostingReportItemRepository leaseLiabilityPostingReportItemRepository,
+        LeaseLiabilityPostingReportItemQueryService leaseLiabilityPostingReportItemQueryService
+    ) {
+        this.leaseLiabilityPostingReportItemService = leaseLiabilityPostingReportItemService;
+        this.leaseLiabilityPostingReportItemRepository = leaseLiabilityPostingReportItemRepository;
+        this.leaseLiabilityPostingReportItemQueryService = leaseLiabilityPostingReportItemQueryService;
+    }
+
+    /**
+     * {@code GET  /lease-liability-posting-report-items} : get all the leaseLiabilityPostingReportItems.
+     *
+     * @param pageable the pagination information.
+     * @param criteria the criteria which the requested entities should match.
+     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and the list of leaseLiabilityPostingReportItems in body.
+     */
+    @GetMapping("/lease-liability-posting-report-items")
+    public ResponseEntity<List<LeaseLiabilityPostingReportItemDTO>> getAllLeaseLiabilityPostingReportItems(
+        LeaseLiabilityPostingReportItemCriteria criteria,
+        Pageable pageable
+    ) {
+        log.debug("REST request to get LeaseLiabilityPostingReportItems by criteria: {}", criteria);
+        Page<LeaseLiabilityPostingReportItemDTO> page = leaseLiabilityPostingReportItemQueryService.findByCriteria(criteria, pageable);
+        HttpHeaders headers = PaginationUtil.generatePaginationHttpHeaders(ServletUriComponentsBuilder.fromCurrentRequest(), page);
+        return ResponseEntity.ok().headers(headers).body(page.getContent());
+    }
+
+    /**
+     * {@code GET  /lease-liability-posting-report-items/count} : count all the leaseLiabilityPostingReportItems.
+     *
+     * @param criteria the criteria which the requested entities should match.
+     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and the count in body.
+     */
+    @GetMapping("/lease-liability-posting-report-items/count")
+    public ResponseEntity<Long> countLeaseLiabilityPostingReportItems(LeaseLiabilityPostingReportItemCriteria criteria) {
+        log.debug("REST request to count LeaseLiabilityPostingReportItems by criteria: {}", criteria);
+        return ResponseEntity.ok().body(leaseLiabilityPostingReportItemQueryService.countByCriteria(criteria));
+    }
+
+    /**
+     * {@code GET  /lease-liability-posting-report-items/:id} : get the "id" leaseLiabilityPostingReportItem.
+     *
+     * @param id the id of the leaseLiabilityPostingReportItemDTO to retrieve.
+     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body the leaseLiabilityPostingReportItemDTO, or with status {@code 404 (Not Found)}.
+     */
+    @GetMapping("/lease-liability-posting-report-items/{id}")
+    public ResponseEntity<LeaseLiabilityPostingReportItemDTO> getLeaseLiabilityPostingReportItem(@PathVariable Long id) {
+        log.debug("REST request to get LeaseLiabilityPostingReportItem : {}", id);
+        Optional<LeaseLiabilityPostingReportItemDTO> leaseLiabilityPostingReportItemDTO = leaseLiabilityPostingReportItemService.findOne(
+            id
+        );
+        return ResponseUtil.wrapOrNotFound(leaseLiabilityPostingReportItemDTO);
+    }
+
+    /**
+     * {@code SEARCH  /_search/lease-liability-posting-report-items?query=:query} : search for the leaseLiabilityPostingReportItem corresponding
+     * to the query.
+     *
+     * @param query the query of the leaseLiabilityPostingReportItem search.
+     * @param pageable the pagination information.
+     * @return the result of the search.
+     */
+    @GetMapping("/_search/lease-liability-posting-report-items")
+    public ResponseEntity<List<LeaseLiabilityPostingReportItemDTO>> searchLeaseLiabilityPostingReportItems(
+        @RequestParam String query,
+        Pageable pageable
+    ) {
+        log.debug("REST request to search for a page of LeaseLiabilityPostingReportItems for query {}", query);
+        Page<LeaseLiabilityPostingReportItemDTO> page = leaseLiabilityPostingReportItemService.search(query, pageable);
+        HttpHeaders headers = PaginationUtil.generatePaginationHttpHeaders(ServletUriComponentsBuilder.fromCurrentRequest(), page);
+        return ResponseEntity.ok().headers(headers).body(page.getContent());
+    }
+}
---
+++ src/main/java/io/github/erp/web/rest/LeaseLiabilityReportItemResource.java
@@ -0,0 +1,135 @@
+package io.github.erp.web.rest;
+
+/*-
+ * Erp System - Mark X No 9 (Jehoiada Series) Server ver 1.8.1
+ * Copyright © 2021 - 2024 Edwin Njeru and the ERP System Contributors (mailnjeru@gmail.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import static org.elasticsearch.index.query.QueryBuilders.*;
+
+import io.github.erp.repository.LeaseLiabilityReportItemRepository;
+import io.github.erp.service.LeaseLiabilityReportItemQueryService;
+import io.github.erp.service.LeaseLiabilityReportItemService;
+import io.github.erp.service.criteria.LeaseLiabilityReportItemCriteria;
+import io.github.erp.service.dto.LeaseLiabilityReportItemDTO;
+import io.github.erp.web.rest.errors.BadRequestAlertException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.StreamSupport;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import tech.jhipster.web.util.HeaderUtil;
+import tech.jhipster.web.util.PaginationUtil;
+import tech.jhipster.web.util.ResponseUtil;
+
+/**
+ * REST controller for managing {@link io.github.erp.domain.LeaseLiabilityReportItem}.
+ */
+@RestController
+@RequestMapping("/api")
+public class LeaseLiabilityReportItemResource {
+
+    private final Logger log = LoggerFactory.getLogger(LeaseLiabilityReportItemResource.class);
+
+    private final LeaseLiabilityReportItemService leaseLiabilityReportItemService;
+
+    private final LeaseLiabilityReportItemRepository leaseLiabilityReportItemRepository;
+
+    private final LeaseLiabilityReportItemQueryService leaseLiabilityReportItemQueryService;
+
+    public LeaseLiabilityReportItemResource(
+        LeaseLiabilityReportItemService leaseLiabilityReportItemService,
+        LeaseLiabilityReportItemRepository leaseLiabilityReportItemRepository,
+        LeaseLiabilityReportItemQueryService leaseLiabilityReportItemQueryService
+    ) {
+        this.leaseLiabilityReportItemService = leaseLiabilityReportItemService;
+        this.leaseLiabilityReportItemRepository = leaseLiabilityReportItemRepository;
+        this.leaseLiabilityReportItemQueryService = leaseLiabilityReportItemQueryService;
+    }
+
+    /**
+     * {@code GET  /lease-liability-report-items} : get all the leaseLiabilityReportItems.
+     *
+     * @param pageable the pagination information.
+     * @param criteria the criteria which the requested entities should match.
+     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and the list of leaseLiabilityReportItems in body.
+     */
+    @GetMapping("/lease-liability-report-items")
+    public ResponseEntity<List<LeaseLiabilityReportItemDTO>> getAllLeaseLiabilityReportItems(
+        LeaseLiabilityReportItemCriteria criteria,
+        Pageable pageable
+    ) {
+        log.debug("REST request to get LeaseLiabilityReportItems by criteria: {}", criteria);
+        Page<LeaseLiabilityReportItemDTO> page = leaseLiabilityReportItemQueryService.findByCriteria(criteria, pageable);
+        HttpHeaders headers = PaginationUtil.generatePaginationHttpHeaders(ServletUriComponentsBuilder.fromCurrentRequest(), page);
+        return ResponseEntity.ok().headers(headers).body(page.getContent());
+    }
+
+    /**
+     * {@code GET  /lease-liability-report-items/count} : count all the leaseLiabilityReportItems.
+     *
+     * @param criteria the criteria which the requested entities should match.
+     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and the count in body.
+     */
+    @GetMapping("/lease-liability-report-items/count")
+    public ResponseEntity<Long> countLeaseLiabilityReportItems(LeaseLiabilityReportItemCriteria criteria) {
+        log.debug("REST request to count LeaseLiabilityReportItems by criteria: {}", criteria);
+        return ResponseEntity.ok().body(leaseLiabilityReportItemQueryService.countByCriteria(criteria));
+    }
+
+    /**
+     * {@code GET  /lease-liability-report-items/:id} : get the "id" leaseLiabilityReportItem.
+     *
+     * @param id the id of the leaseLiabilityReportItemDTO to retrieve.
+     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body the leaseLiabilityReportItemDTO, or with status {@code 404 (Not Found)}.
+     */
+    @GetMapping("/lease-liability-report-items/{id}")
+    public ResponseEntity<LeaseLiabilityReportItemDTO> getLeaseLiabilityReportItem(@PathVariable Long id) {
+        log.debug("REST request to get LeaseLiabilityReportItem : {}", id);
+        Optional<LeaseLiabilityReportItemDTO> leaseLiabilityReportItemDTO = leaseLiabilityReportItemService.findOne(id);
+        return ResponseUtil.wrapOrNotFound(leaseLiabilityReportItemDTO);
+    }
+
+    /**
+     * {@code SEARCH  /_search/lease-liability-report-items?query=:query} : search for the leaseLiabilityReportItem corresponding
+     * to the query.
+     *
+     * @param query the query of the leaseLiabilityReportItem search.
+     * @param pageable the pagination information.
+     * @return the result of the search.
+     */
+    @GetMapping("/_search/lease-liability-report-items")
+    public ResponseEntity<List<LeaseLiabilityReportItemDTO>> searchLeaseLiabilityReportItems(
+        @RequestParam String query,
+        Pageable pageable
+    ) {
+        log.debug("REST request to search for a page of LeaseLiabilityReportItems for query {}", query);
+        Page<LeaseLiabilityReportItemDTO> page = leaseLiabilityReportItemService.search(query, pageable);
+        HttpHeaders headers = PaginationUtil.generatePaginationHttpHeaders(ServletUriComponentsBuilder.fromCurrentRequest(), page);
+        return ResponseEntity.ok().headers(headers).body(page.getContent());
+    }
+}
