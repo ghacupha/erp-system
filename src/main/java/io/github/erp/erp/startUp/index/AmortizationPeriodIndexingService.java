c68a41bc0f2a058cfb6d9424ffc5d552062f5e3d
Edwin Njeru <mailnjeru@gmail.com>
2024-11-20 14:51:11 +0300

    move index files to startup folders

---
+++ src/main/java/io/github/erp/erp/startUp/index/AmortizationPeriodIndexingService.java
@@ -0,0 +1,123 @@
+package io.github.erp.erp.startUp.index;
+
+/*-
+ * Erp System - Mark X No 10 (Jehoiada Series) Server ver 1.8.2
+ * Copyright © 2021 - 2024 Edwin Njeru and the ERP System Contributors (mailnjeru@gmail.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+import com.google.common.collect.ImmutableList;
+import io.github.erp.domain.AmortizationPeriod;
+import io.github.erp.erp.startUp.index.engine_v1.IndexingServiceChainSingleton;
+import io.github.erp.erp.startUp.index.engine_v2.AbstractStartUpBatchedIndexService;
+import io.github.erp.internal.IndexProperties;
+import io.github.erp.repository.search.AmortizationPeriodSearchRepository;
+import io.github.erp.service.AmortizationPeriodService;
+import io.github.erp.service.mapper.AmortizationPeriodMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Pageable;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+@Service
+@Transactional
+public class AmortizationPeriodIndexingService extends AbstractStartUpBatchedIndexService<AmortizationPeriod> {
+
+
+    private static final String TAG = "AmortizationPeriodIndex";
+    private static final Logger log = LoggerFactory.getLogger(TAG);
+
+    private final AmortizationPeriodMapper mapper;
+    private final AmortizationPeriodService service;
+    private final AmortizationPeriodSearchRepository searchRepository;
+
+    public AmortizationPeriodIndexingService(
+        IndexProperties indexProperties,
+        AmortizationPeriodMapper mapper,
+        AmortizationPeriodService service,
+        AmortizationPeriodSearchRepository searchRepository) {
+        super(indexProperties, indexProperties.getRebuild());
+        this.mapper = mapper;
+        this.service = service;
+        this.searchRepository = searchRepository;
+    }
+
+    /**
+     * This method is called to register a service which is to respond to the callback
+     */
+    @Override
+    public void register() {
+
+        log.info("Registering {} Service", TAG);
+
+        IndexingServiceChainSingleton.getInstance().registerService(this);
+    }
+
+    private static final Lock reindexLock = new ReentrantLock();
+
+    @Async
+    public void index() {
+        try {
+            reindexLock.lockInterruptibly();
+
+            int batches = indexerSequence();
+
+            log.info("{} batches processed", batches);
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            reindexLock.unlock();
+        }
+    }
+
+    private int indexerSequence() {
+        log.info("Initiating {} build sequence", TAG);
+        long startup = System.currentTimeMillis();
+        log.trace("{} initiated and ready for queries. Index build has taken {} milliseconds", TAG, System.currentTimeMillis() - startup);
+
+        return processInBatchesOf(300);
+    }
+
+    @Override
+    public void tearDown() {
+
+        if (reindexLock.tryLock()) {
+            this.searchRepository.deleteAll();
+        } else {
+            log.trace("{} ReIndexer: Concurrent reindexing attempt", TAG);
+        }
+    }
+
+    @Override
+    protected List<AmortizationPeriod> getItemsForIndexing() {
+        return service.findAll(Pageable.unpaged())
+            .stream()
+            .map(mapper::toEntity)
+            .filter(entity -> !searchRepository.existsById(entity.getId()))
+            .collect(ImmutableList.toImmutableList());
+    }
+
+    @Override
+    protected void processBatchIndex(List<AmortizationPeriod> batch) {
+
+        this.searchRepository.saveAll(batch);
+    }
+}
