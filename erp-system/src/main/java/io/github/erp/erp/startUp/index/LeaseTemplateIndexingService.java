0fdd05f331a69c5dfac78f9767920b6cd98f1957
Edwin Njeru <mailnjeru@gmail.com>
2026-01-06 14:21:51 +0300

    implement query to update lease-template

---
+++ erp-system/src/main/java/io/github/erp/erp/startUp/index/LeaseTemplateIndexingService.java
@@ -0,0 +1,121 @@
+
+/*-
+ * Erp System - Mark X No 11 (Jehoiada Series) Server ver 1.8.3
+ * Copyright © 2021 - 2024 Edwin Njeru and the ERP System Contributors (mailnjeru@gmail.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package io.github.erp.erp.startUp.index;
+
+/*-
+ * Erp System - Mark X No 10 (Jehoiada Series) Server ver 1.8.2
+ * Copyright © 2021 - 2024 Edwin Njeru and the ERP System Contributors (mailnjeru@gmail.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+import com.google.common.collect.ImmutableList;
+import io.github.erp.erp.startUp.index.engine_v1.AbstractStartupRegisteredIndexService;
+import io.github.erp.erp.startUp.index.engine_v1.IndexingServiceChainSingleton;
+import io.github.erp.internal.IndexProperties;
+import io.github.erp.repository.search.LeaseTemplateSearchRepository;
+import io.github.erp.service.LeaseTemplateService;
+import io.github.erp.service.mapper.LeaseTemplateMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Pageable;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+@Service
+@Transactional
+public class LeaseTemplateIndexingService extends AbstractStartupRegisteredIndexService {
+
+    private static final String TAG = "LeaseTemplateScheduleItemIndex";
+    private static final Logger log = LoggerFactory.getLogger(LeaseTemplateIndexingService.class);
+
+    private final LeaseTemplateService service;
+    private final LeaseTemplateMapper mapper;
+    private final LeaseTemplateSearchRepository searchRepository;
+
+    public LeaseTemplateIndexingService (IndexProperties indexProperties, LeaseTemplateService service, LeaseTemplateMapper mapper, LeaseTemplateSearchRepository searchRepository) {
+        super(indexProperties, indexProperties.getRebuild());
+        this.service = service;
+        this.mapper = mapper;
+        this.searchRepository = searchRepository;
+    }
+
+    /**
+     * This method is called to register a service which is to respond to the callback
+     */
+    @Override
+    public void register() {
+
+        log.info("Registering {} Service", TAG);
+
+        IndexingServiceChainSingleton.getInstance().registerService(this);
+    }
+
+    private static final Lock reindexLock = new ReentrantLock();
+
+    @Async
+    public void index() {
+        try {
+            reindexLock.lockInterruptibly();
+
+            indexerSequence();
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            reindexLock.unlock();
+        }
+    }
+
+    private void indexerSequence() {
+        log.info("Initiating {} build sequence", TAG);
+        long startup = System.currentTimeMillis();
+        this.searchRepository.saveAll(
+            service.findAll(Pageable.unpaged())
+                .stream()
+                .map(mapper::toEntity)
+                .filter(entity -> !searchRepository.existsById(entity.getId()))
+                .collect(ImmutableList.toImmutableList()));
+        log.trace("{} initiated and ready for queries. Index build has taken {} milliseconds", TAG, System.currentTimeMillis() - startup);
+    }
+
+    @Override
+    public void tearDown() {
+
+        if (reindexLock.tryLock()) {
+            this.searchRepository.deleteAll();
+        } else {
+            log.trace("{} ReIndexer: Concurrent reindexing attempt", TAG);
+        }
+    }
+}
